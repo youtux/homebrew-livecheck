a5b4bbdcad0665784a2c35e3adadc0801361a71e
Alessio Bogon <youtux@gmail.com>
2014-10-21 23:29:12 +0200

    added sha1 to the formula

--- Formula/brew-livecheck.rb
+++ Formula/brew-livecheck.rb
@@ -3,6 +3,7 @@ require 'formula'
 class BrewLivecheck < Formula
   homepage 'https://github.com/youtux/homebrew-livecheck'
   url 'https://github.com/youtux/homebrew-livecheck/archive/0.1.tar.gz'
+  sha1 'c0886e147a9459f4c5e79ef1030d07c8e39a5b70'
   head 'https://github.com/youtux/homebrew-livecheck.git'
 
   skip_clean 'bin'
