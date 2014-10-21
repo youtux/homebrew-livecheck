2278f111dfd43ce3fa4d770a9e88b1973abb5c1f
Alessio Bogon <youtux@gmail.com>
2014-10-22 00:51:38 +0200

    refixed structure

--- bin/brew-livecheck.rb
+++ bin/brew-livecheck.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift(File.expand_path('../../', Pathname.new(__FILE__).realpath))
-require "livecheck/utils"
+require "lib/utils"
 
 usage = <<EOF
     brew livecheck formula1 formula2 ...
@@ -21,10 +21,7 @@ def check_flags flags
 end
 
 def latest_version formula
-    f_basename = File.basename formula.path
-    if Dir["../Livecheckables/*.rb"].map{ |f| File.basename f }.include? f_basename
-      require "../Livecheckables/#{f_basename}"
-    end
+    require "Livecheckables/#{formula}" if File.exists? "../Livecheckables/#{formula}.rb"
 
     if formula.respond_to? :get_latest_version
       formula.get_latest_version
@@ -37,7 +34,6 @@ def latest_version formula
 end
 
 if check_flags ['-d']
-
     puts Pathname.new(__FILE__).realpath
     puts $LOAD_PATH
 end
@@ -53,7 +49,7 @@ else
     begin
         latest = latest_version(f)
         puts latest unless check_flags ['-n', '--only-newer'] and f.version <= latest
-    rescue Exception => e
+    rescue TypeError => e
         puts e
     end
   end
