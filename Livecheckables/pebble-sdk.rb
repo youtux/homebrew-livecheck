24e7da3ef6aa530ad15bd58e6f764f3d93849c8b
Alessio Bogon <youtux@gmail.com>
2015-01-02 15:41:08 +0100

    pebble-sdk: modernize

--- Livecheckables/pebble-sdk.rb
+++ Livecheckables/pebble-sdk.rb
@@ -1,11 +1,5 @@
-require 'formula'
-
 class PebbleSdk
   def livecheck
-    require 'net/http'
-    url = "https://developer.getpebble.com/sdk/"
-
-    page = Net::HTTP.get(URI(url))
-    version = page.match('Current Version: (.*) \(')[1]
+    page_match("https://developer.getpebble.com/sdk/", 'Current Version: (.*) \(')
   end
 end
