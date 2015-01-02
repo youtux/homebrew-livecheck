e2aa4a6c9d052dd71c4a3781ddb0fbb23a29bb5a
Alessio Bogon <youtux@gmail.com>
2015-01-02 15:39:26 +0100

    watch: update

--- Livecheckables/watch.rb
+++ Livecheckables/watch.rb
@@ -1,12 +1,10 @@
-require "formula"
+require "net/http"
 
 class Watch
   def livecheck
-    require "net/http"
     url = "http://sourceforge.net/projects/procps-ng/files/latest/download"
-
     redirect = Net::HTTP.get_response(URI(url))['location']
 
-    Version.detect(URI(redirect).path)
+    return Version.detect(URI(redirect).path)
   end
 end
