d24d3cb2af2d6272346ea31251d265ecb5bf86bc
Alessio Bogon <youtux@gmail.com>
2015-01-02 15:39:11 +0100

    New utils helper

--- livecheck/utils.rb
+++ livecheck/utils.rb
@@ -1,6 +1,13 @@
+require "net/http"
+
 def git_tags repo_url
   out = `git ls-remote --tags #{repo_url}`
   out.gsub!(/^.*\trefs\/tags\//, "")
   out.gsub!(/\^\{\}$/, "")
-  out.split("\n").uniq.sort
+  return out.split("\n").uniq.sort
+end
+
+def page_match(url, pattern)
+  page = Net::HTTP.get(URI(url))
+  return version = page.match(pattern)[1]
 end
