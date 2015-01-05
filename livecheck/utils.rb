762a4950cbe81e23f5cb184ca26482f59a7717bc
Alessio Bogon <youtux@gmail.com>
2015-01-05 19:58:02 +0100

    utils: code beautification

--- livecheck/utils.rb
+++ livecheck/utils.rb
@@ -1,22 +1,20 @@
 require "net/http"
 
-def git_tags(repo_url, filter=nil)
+def git_tags(repo_url, filter = nil)
   raw_tags = `git ls-remote --tags #{repo_url}`
-  raw_tags.gsub!(/^.*\trefs\/tags\//, "")
+  raw_tags.gsub!(%r{^.*\trefs/tags/}, "")
   raw_tags.gsub!(/\^\{\}$/, "")
 
   tags = raw_tags.split("\n").uniq.sort
-  if filter
-    tags.select! { |t| t =~ filter}
-  end
-  return tags
+  tags.select! { |t| t =~ filter } if filter
+  tags
 end
 
 def page_matches(url, regex)
-  puts %Q[Using page_match("#{url}", "#{regex}")] if ARGV.debug?
+  puts %[Using page_match("#{url}", "#{regex}")] if ARGV.debug?
   page = open(url).read
   matches = page.scan(regex)
   puts matches if ARGV.debug?
-  return matches.map { |version_s| version_s[0] }.uniq
+  matches.map { |version_s| version_s[0] }.uniq
 end
 
