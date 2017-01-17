f468a32e29dd0fe6843d5cd326aeedf300b00600
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2017-01-17 01:13:22 -0500

    Update livecheckables of virt-manager dependencies (#12)

--- Livecheckables/spice-gtk.rb
+++ Livecheckables/spice-gtk.rb
@@ -1,4 +1,4 @@
 class SpiceGtk < Formula
-  livecheck :url => "http://www.spice-space.org/download/gtk/",
+  livecheck :url => "https://www.spice-space.org/download/gtk/",
             :regex => /spice-gtk-([\d.]+)\.tar\.bz2/
 end
--- Livecheckables/spice-protocol.rb
+++ Livecheckables/spice-protocol.rb
@@ -1,4 +1,4 @@
 class SpiceProtocol < Formula
-  livecheck :url => "http://www.spice-space.org/download/releases/",
+  livecheck :url => "https://www.spice-space.org/download/releases/",
             :regex => /spice-protocol-([\d.]+)\.tar\.bz2/
 end
