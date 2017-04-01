bf3c3797bb13234cb4ae691cbb7698d5b5d375a5
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2017-04-01 11:08:15 -0400

    Update some livecheckable urls (#15)

--- Livecheckables/virt-viewer.rb
+++ Livecheckables/virt-viewer.rb
@@ -1,4 +1,4 @@
 class VirtViewer < Formula
-  livecheck :url => "https://fedorahosted.org/released/virt-viewer/",
+  livecheck :url => "https://virt-manager.org/download/sources/virt-viewer/",
             :regex => /virt-viewer-([\d.]+)\.tar\.gz/
 end
