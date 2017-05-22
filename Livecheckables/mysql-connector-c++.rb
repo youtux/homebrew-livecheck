2e6e7c6394c1c9ca0b8faea44fff85a884e81e67
ilovezfs <ilovezfs@icloud.com>
2017-05-21 22:49:33 -0700

    mysql-connector-c++: add livecheck

---
+++ Livecheckables/mysql-connector-c++.rb
@@ -0,0 +1,4 @@
+class MysqlConnectorCxx
+  livecheck :url => "http://dev.mysql.com/downloads/connector/cpp/",
+            :regex => /href="\/downloads\/gpg\/\?file=mysql-connector-c%2B%2B-(\d+.\d+.\d+)-/
+end
