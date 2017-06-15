3d2fe033a40a7edb5f8cc38c39e4ecf7b2067f21
ilovezfs <ilovezfs@icloud.com>
2017-06-14 23:10:08 -0700

    jsonschema2pojo: add livecheck

---
+++ Livecheckables/jsonschema2pojo.rb
@@ -0,0 +1,4 @@
+class Jsonschema2pojo
+  livecheck :url => "https://github.com/joelittlejohn/jsonschema2pojo/releases",
+            :regex => %r{releases/download/jsonschema2pojo-.*/jsonschema2pojo-([0-9,\.]+)\.tar}
+end
