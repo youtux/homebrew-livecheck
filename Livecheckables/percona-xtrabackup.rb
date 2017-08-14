cca9094ba760dae9673de67a86478c896ad78a38
ilovezfs <ilovezfs@icloud.com>
2017-08-14 02:26:36 -0700

    percona-xtrabackup: add livecheck

---
+++ Livecheckables/percona-xtrabackup.rb
@@ -0,0 +1,4 @@
+class PerconaXtrabackup
+  livecheck :url => "https://www.percona.com/downloads/XtraBackup/LATEST/",
+            :regex => %r{value="XtraBackup/Percona-XtraBackup-([0-9\-\.]+)"}
+end
