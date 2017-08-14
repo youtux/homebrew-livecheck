4cffa85d05c362df1a8b73f656d5f1ba46a5d83a
ilovezfs <ilovezfs@icloud.com>
2017-08-14 02:26:33 -0700

    gnupg-pkcs11-scd: add livecheck

---
+++ Livecheckables/gnupg-pkcs11-scd.rb
@@ -0,0 +1,4 @@
+class GnupgPkcs11Scd
+  livecheck :url => "https://github.com/alonbl/gnupg-pkcs11-scd/releases",
+            :regex => %r{href="/alonbl/gnupg-pkcs11-scd/releases/tag/gnupg-pkcs11-scd-([0-9,\.]+)"}
+end
