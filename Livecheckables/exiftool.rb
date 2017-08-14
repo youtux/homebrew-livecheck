35b0565c851a4011f7d51d367fdacb00afc533ca
ilovezfs <ilovezfs@icloud.com>
2017-08-14 02:26:33 -0700

    exiftool: add livecheck

---
+++ Livecheckables/exiftool.rb
@@ -0,0 +1,4 @@
+class Exiftool
+  livecheck :url => "https://www.sno.phy.queensu.ca/~phil/exiftool/history.html",
+            :regex => /production release is <a href="Image-ExifTool-([0-9\.]+)\.t/
+end
