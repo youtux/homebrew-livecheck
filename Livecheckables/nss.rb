ec45d7431a1ea582c65d4a2d101beab2db09abeb
ilovezfs <ilovezfs@icloud.com>
2017-08-14 02:26:35 -0700

    nss: add livecheck

---
+++ Livecheckables/nss.rb
@@ -0,0 +1,4 @@
+class Nss
+  livecheck :url => "https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS/NSS_Releases",
+            :regex => %r{href="/en-US/docs/Mozilla/Projects/NSS/NSS_([0-9\.]+)_release_notes"}
+end
