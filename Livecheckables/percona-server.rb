c02b39c148aad262183de1ec40535ff5d242b654
ilovezfs <ilovezfs@icloud.com>
2017-08-14 02:26:36 -0700

    percona-server: add livecheck

---
+++ Livecheckables/percona-server.rb
@@ -0,0 +1,4 @@
+class PerconaServer
+  livecheck :url => "https://www.percona.com/downloads/Percona-Server-LATEST/",
+            :regex => %r{value="Percona-Server-LATEST/Percona-Server-([0-9\-\.]+)"}
+end
