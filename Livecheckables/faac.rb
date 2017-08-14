bca82c0cdbdeac9a1ee7ce1e5b57371daa398ece
Jon Hermansen <jon@jh86.org>
2017-08-14 08:40:32 -0400

    Fix livechecks that were failing due to redirects

--- Livecheckables/faac.rb
+++ Livecheckables/faac.rb
@@ -1,4 +1,4 @@
 class Faac
-  livecheck :url => "http://sourceforge.net/projects/faac/files/faac-src/",
-            :regex => /faac-(\d+(?:\.\d+)*)\.7z/
+  livecheck :url => "https://sourceforge.net/projects/faac/files/faac-src/",
+            :regex => /faac-(\d+(?:\.\d+)*)\.tar\.gz/
 end
