a1fa5231a07af46e967dcfac2a898bdde450600f
ilovezfs <ilovezfs@icloud.com>
2017-09-03 10:09:45 -0700

    sqlite: update livecheck

--- Livecheckables/sqlite.rb
+++ Livecheckables/sqlite.rb
@@ -1,4 +1,4 @@
 class Sqlite
-  livecheck :url => "https://sqlite.org/download.html",
-            :regex => /version (\d(?:.\d)*)/
+  livecheck :url => "https://sqlite.org/news.html",
+            :regex => /(3(?:\.[0-9]+)+)/
 end
