9516b146b1a15f52aa33bbc83e7cd447e89ec1e6
ilovezfs <ilovezfs@icloud.com>
2017-09-03 10:05:28 -0700

    bcftools: add livecheck

---
+++ Livecheckables/bcftools.rb
@@ -0,0 +1,4 @@
+class Bcftools
+  livecheck :url => "https://github.com/samtools/bcftools/releases",
+            :regex => %r{href="/samtools/bcftools/releases/download/.*/bcftools-([0-9\.]+).tar.bz2"}
+end
