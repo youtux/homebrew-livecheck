119791778d091583e9efcc1bf8e0349f8ad583dc
ilovezfs <ilovezfs@icloud.com>
2017-09-05 12:15:53 -0700

    ats2-postiats: add livecheck

---
+++ Livecheckables/ats2-postiats.rb
@@ -0,0 +1,4 @@
+class Ats2Postiats
+  livecheck :url => "http://www.ats-lang.org/Downloads.html",
+            :regex => %r{The current <em>stable</em> release of ATS2 is at\n<a href="http://sourceforge.net/projects/ats2-lang/download">ATS2-([0-9\.]+)</a>}
+end
