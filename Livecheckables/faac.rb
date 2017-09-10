e7df39403f62fc3494607e83e3c14d562e44c904
ilovezfs <ilovezfs@icloud.com>
2017-09-10 11:27:59 -0700

    faac: update livecheck

--- Livecheckables/faac.rb
+++ Livecheckables/faac.rb
@@ -1,4 +1,4 @@
 class Faac
   livecheck :url => "http://sourceforge.net/projects/faac/files/faac-src/",
-            :regex => /faac-(\d+(?:\.\d+)*)\.7z/
+            :regex => /faac-(\d+(?:\.\d+)*)\.t/
 end
