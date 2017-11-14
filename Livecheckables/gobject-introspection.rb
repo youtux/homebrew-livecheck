cdeb11e59e5b9dd06f4dcbc71a8fbfa1d4cd87ba
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    gobject-introspection: update livecheck

--- Livecheckables/gobject-introspection.rb
+++ Livecheckables/gobject-introspection.rb
@@ -1,4 +1,4 @@
 class GobjectIntrospection
-  livecheck :url => "https://download.gnome.org/sources/gobject-introspection/1.52/",
+  livecheck :url => "https://download.gnome.org/sources/gobject-introspection/1.54/",
             :regex => /LATEST-IS-([\d.]+)"/
 end
