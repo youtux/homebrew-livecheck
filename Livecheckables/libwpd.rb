696bc881b4920e296b3bafe6499d49dd28e48d88
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    libwpd: update livecheck

--- Livecheckables/libwpd.rb
+++ Livecheckables/libwpd.rb
@@ -1,4 +1,4 @@
 class Libwpd
   livecheck :url => "http://dev-www.libreoffice.org/src/",
-            :regex => /.*href=\"libwpd-([0-9\.\-]+)\.tar.[bgz2]+\"/
+            :regex => /.*href=\"libwpd-([0-9\.\-]+)\.t/
 end
