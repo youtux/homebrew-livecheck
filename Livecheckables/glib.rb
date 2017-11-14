f3e5a20961d77ce5205971b3a557afc8a23d2f11
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    glib: update livecheck

--- Livecheckables/glib.rb
+++ Livecheckables/glib.rb
@@ -1,4 +1,4 @@
 class Glib
-  livecheck :url => "https://download.gnome.org/sources/glib/2.52/",
+  livecheck :url => "https://download.gnome.org/sources/glib/2.54/",
             :regex => /href="LATEST-IS-([\d.]+)"/
 end
