24eb14fa3fbb7e295a9a8853dd0616fbb369e272
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    unrar: update livecheck

--- Livecheckables/unrar.rb
+++ Livecheckables/unrar.rb
@@ -1,4 +1,4 @@
 class Unrar
-  livecheck :url => "http://www.rarlab.com/rar_add.htm",
-            :regex => %r{href="http://www.rarlab.com/rar/unrarsrc-([0-9,\.]+).tar}
+  livecheck :url => "https://www.rarlab.com/rar_add.htm",
+            :regex => %r{href="rar/unrarsrc-([0-9,\.]+).tar}
 end
