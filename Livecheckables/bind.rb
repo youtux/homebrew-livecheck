69008c12eb46d978bceddd61d49574c21cf91c28
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:58 -0800

    bind: update livecheck

--- Livecheckables/bind.rb
+++ Livecheckables/bind.rb
@@ -1,4 +1,4 @@
 class Bind
-  livecheck :url => "https://www.isc.org/downloads/bind/",
-            :regex => /bind-([A-Za-z0-9\.\-]+)\.t/
+  livecheck :url => "https://www.isc.org/downloads/",
+            :regex => /Early Deployment.*?href=".*?bind-([A-Za-z0-9\.\-]+)\.t/m
 end
