3b3aa638e3ead099a7e42afa26806a4f53c8e63e
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    dcmtk: update livecheck

--- Livecheckables/dcmtk.rb
+++ Livecheckables/dcmtk.rb
@@ -1,4 +1,4 @@
 class Dcmtk
-  livecheck :url => "http://dicom.offis.de/download/dcmtk/snapshot/",
+  livecheck :url => "http://dicom.offis.de/download/dcmtk/release/",
             :regex => /href="dcmtk-([0-9\._]+)\.t/
 end
