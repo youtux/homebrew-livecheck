76f50b25ee205329368c447d31c03b903428b475
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    remake: update livecheck

--- Livecheckables/remake.rb
+++ Livecheckables/remake.rb
@@ -1,4 +1,4 @@
 class Remake
   livecheck :url => "https://sourceforge.net/projects/bashdb/files/remake/",
-            :regex => /<span>Download remake-([0-9\.\+a-z]+)\.t/
+            :regex => /<span>Download remake-([0-9\.\+\-a-z]+)\.t/
 end
