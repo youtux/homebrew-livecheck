0eacf410a4dbcf2fd0cf2bdbc4f08f9dc1c8170f
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    nagios: update livecheck

--- Livecheckables/nagios.rb
+++ Livecheckables/nagios.rb
@@ -1,4 +1,4 @@
 class Nagios
-  livecheck :url => "https://sourceforge.net/projects/nagios/files/",
-            :regex => %r{href=".*/latest.*nagios-.*/nagios-([0-9,\.]+)\.tar}
+  livecheck :url => "https://sourceforge.net/projects/nagios/",
+            :regex => %r{/.*nagios-.*/nagios-([0-9,\.]+)\.tar}
 end
