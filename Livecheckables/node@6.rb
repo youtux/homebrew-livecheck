a4d4dd404d97730f275c1da5027612359efb932f
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:28:59 -0800

    node@6: update livecheck

--- Livecheckables/node@6.rb
+++ Livecheckables/node@6.rb
@@ -1,4 +1,4 @@
 class NodeAT6
   livecheck :url => "https://nodejs.org/en/download/releases/",
-            :regex => %r{<td data-label="Version">Node.js v(6\.[0-9\.]+)</td>}
+            :regex => %r{<td data-label="Version">Node.js (6\.[0-9\.]+)</td>}
 end
