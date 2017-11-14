5e645a0840b1938598d173c373dd4f447bfc3045
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:13:19 -0800

    hypre: add livecheck

---
+++ Livecheckables/hypre.rb
@@ -0,0 +1,4 @@
+class Hypre
+  livecheck :url => "https://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/software",
+            :regex => %r{href=".*?/hypre-([0-9\.]+)\.t}
+end
