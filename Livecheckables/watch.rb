4f4adfe7b802840800773df7dddc95603f3f6d49
ilovezfs <ilovezfs@icloud.com>
2017-11-14 10:29:00 -0800

    watch: update livecheck

--- Livecheckables/watch.rb
+++ Livecheckables/watch.rb
@@ -1,4 +1,4 @@
 class Watch
   livecheck :url => "https://gitlab.com/procps-ng/procps/tags",
-            :regex => %r{/procps-ng/procps/repository/archive.zip\?ref=v([0-9.]*)}
+            :regex => %r{href="/procps-ng/procps/tags/v([0-9.]*)}
 end
