9b38c226598154fca629334430b70e73f21ae5ae
ilovezfs <ilovezfs@icloud.com>
2018-01-01 05:17:05 -0800

    librealsense: add livecheck

---
+++ Livecheckables/librealsense.rb
@@ -0,0 +1,4 @@
+class Librealsense
+  livecheck :url => "https://github.com/IntelRealSense/librealsense/releases",
+            :regex => %r{href="/IntelRealSense/librealsense/tree/v?([0-9\.]+)}
+end
