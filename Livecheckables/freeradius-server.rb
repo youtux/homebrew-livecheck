089fa3014644805de43df4ee9b7273c2138c4d7a
ilovezfs <ilovezfs@icloud.com>
2018-02-08 23:06:42 -0800

    freeradius-server: add livecheck

---
+++ Livecheckables/freeradius-server.rb
@@ -0,0 +1,4 @@
+class FreeradiusServer
+  livecheck :url => "https://build.opensuse.org/package/show/openSUSE:Factory/freeradius-server",
+            :regex => /freeradius-server-([0-9\.]+)\.t/
+end
