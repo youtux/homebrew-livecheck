422d8c6760a19b97932c66f31aabfc3580d1b0c8
ilovezfs <ilovezfs@icloud.com>
2018-02-08 23:04:02 -0800

    openssl: update livecheck

--- Livecheckables/openssl.rb
+++ Livecheckables/openssl.rb
@@ -1,4 +1,4 @@
 class Openssl
-  livecheck :url => "https://github.com/openssl/openssl.git",
-            :regex => /OpenSSL_(\d+_\d+_\d+.*)/
+  livecheck :url => "https://www.openssl.org/source/",
+            :regex => /href="openssl-(1\.0[0-9a-z\.]+)\.t/
 end
