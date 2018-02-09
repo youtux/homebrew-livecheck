972d2af6974ad5996bdd2ef29dd9239d3ea222c4
ilovezfs <ilovezfs@icloud.com>
2018-02-08 23:04:01 -0800

    entr: update livecheck

--- Livecheckables/entr.rb
+++ Livecheckables/entr.rb
@@ -1,4 +1,4 @@
 class Entr
-  livecheck :url => "http://entrproject.org/code/",
-            :regex => /href="entr-([0-9\.]+)\.t/
+  livecheck :url => "http://entrproject.org/",
+            :regex => %r{href=.*?/entr-([0-9\.]+)\.t}
 end
