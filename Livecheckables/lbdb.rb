658e14c84446fe69efbfe31efcdef56c4b6a5982
ilovezfs <ilovezfs@icloud.com>
2018-02-08 23:04:01 -0800

    lbdb: update livecheck

--- Livecheckables/lbdb.rb
+++ Livecheckables/lbdb.rb
@@ -1,4 +1,4 @@
 class Lbdb
-  livecheck :url => "https://www.spinnaker.de/lbdb/",
-            :regex => %r{href="\.\./debian/lbdb_([0-9\.]+)\.t.*?">source package}
+  livecheck :url => "https://www.spinnaker.de/lbdb/download/",
+            :regex => /href="lbdb_([0-9\.]+)\.t/
 end
