24cbae80f0fc8cf9eaf5f34244a282306931457f
commitay <commitay@users.noreply.github.com>
2018-03-06 21:03:12 +1000

    percona-server@5.5: remove livecheck

--- Livecheckables/percona-server@5.5.rb
+++
@@ -1,4 +0,0 @@
-class PerconaServerAT55
-  livecheck :url => "https://www.percona.com/downloads/Percona-Server-5.5/LATEST/",
-            :regex => %r{value="Percona-Server-5.5/Percona-Server-([0-9\-\.]+)"}
-end
