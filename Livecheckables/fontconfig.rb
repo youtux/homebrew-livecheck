c18df253164d9bccc2c239b4d5579b3e51d57f7e
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:29 -0700

    fontconfig: update livecheck

--- Livecheckables/fontconfig.rb
+++ Livecheckables/fontconfig.rb
@@ -1,4 +1,4 @@
 class Fontconfig
   livecheck :url => "https://www.freedesktop.org/software/fontconfig/release/",
-            :regex => /fontconfig-(\d+(?:\.\d+)*\.(?:\d|[0-8]\d))\.tar/
+            :regex => /fontconfig-(\d+(?:\.\d+)*\.\d[02468]\.(?:\d|[0-8]\d))\.tar/
 end
