edd59d2587eca3cb0922383f8a1660bbcc979d54
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:29 -0700

    dovecot: update livecheck

--- Livecheckables/dovecot.rb
+++ Livecheckables/dovecot.rb
@@ -1,4 +1,4 @@
 class Dovecot
-  livecheck :url => "https://dovecot.org/releases/2.2/",
+  livecheck :url => "https://dovecot.org/releases/2.3/",
             :regex => /dovecot-(\d+\.\d+([0-9rc.]+)?)\.tar/
 end
