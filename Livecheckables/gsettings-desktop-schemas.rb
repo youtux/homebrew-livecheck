fca008bf25db608d29921176f769820c72b97fd9
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:58:00 -0700

    gsettings-desktop-schemas: update livecheck

--- Livecheckables/gsettings-desktop-schemas.rb
+++ Livecheckables/gsettings-desktop-schemas.rb
@@ -1,4 +1,4 @@
 class GsettingsDesktopSchemas
-  livecheck :url => "https://download.gnome.org/sources/gsettings-desktop-schemas/3.24/",
+  livecheck :url => "https://download.gnome.org/sources/gsettings-desktop-schemas/3.28/",
             :regex => /href="LATEST-IS-([0-9\.]+)"/
 end
