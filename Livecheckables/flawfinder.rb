546daa6778e03af400f99f8b9d7695ccc0262187
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:29 -0700

    flawfinder: update livecheck

--- Livecheckables/flawfinder.rb
+++ Livecheckables/flawfinder.rb
@@ -1,4 +1,4 @@
 class Flawfinder
-  livecheck :url => "http://www.dwheeler.com/flawfinder/",
+  livecheck :url => "https://www.dwheeler.com/flawfinder/",
             :regex => /href="flawfinder-([0-9\.]+)\.t/
 end
