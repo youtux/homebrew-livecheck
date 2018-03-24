4004c9e1abee5203740d5b468d00e8f448d4892a
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:32 -0700

    tomcat: update livecheck

--- Livecheckables/tomcat.rb
+++ Livecheckables/tomcat.rb
@@ -1,4 +1,4 @@
 class Tomcat
-  livecheck :url => "http://apache.panu.it/tomcat/tomcat-8/",
+  livecheck :url => "http://apache.panu.it/tomcat/tomcat-9/",
             :regex => %r{href="v(.*?)/"}
 end
