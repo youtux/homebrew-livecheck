70fa283ad9656862c35429e419f1ac3d91d463e5
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:32 -0700

    libvisio: update livecheck

--- Livecheckables/libvisio.rb
+++ Livecheckables/libvisio.rb
@@ -1,4 +1,4 @@
 class Libvisio
-  livecheck :url => "http://dev-www.libreoffice.org/src/",
+  livecheck :url => "https://dev-www.libreoffice.org/src/",
             :regex => /.*href=\"libvisio-([0-9\.\-]+)\.t/
 end
