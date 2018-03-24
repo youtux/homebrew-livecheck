9f8bc972cf0c3bcfe05d74d0533573eadc0a5e1d
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:30:27 -0700

    joplin: update livecheck

--- Livecheckables/joplin.rb
+++ Livecheckables/joplin.rb
@@ -1,4 +1,4 @@
 class Joplin
   livecheck :url => "https://www.npmjs.com/package/joplin",
-            :regex => %r{<strong>([0-9\.]+)<.*?latest}m
+            :regex => %r{package__sidebarText.*?>([0-9\.]+)</p>}
 end
