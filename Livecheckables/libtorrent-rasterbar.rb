2c193d4c362bc2bb818f9241d46e1bfd2e0fa3bf
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:31 -0700

    libtorrent-rasterbar: update livecheck

--- Livecheckables/libtorrent-rasterbar.rb
+++ Livecheckables/libtorrent-rasterbar.rb
@@ -1,4 +1,4 @@
 class LibtorrentRasterbar
-  livecheck :url => "http://libtorrent.org/",
+  livecheck :url => "https://libtorrent.org/",
             :regex => %r{<tr><th class="docinfo-name">Version:</th>\n<td>([0-9\.]+)</td></tr>}
 end
