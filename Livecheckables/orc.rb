4941fae5c8a33ce418db1ec237fdb450057502fc
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:32 -0700

    orc: update livecheck

--- Livecheckables/orc.rb
+++ Livecheckables/orc.rb
@@ -1,4 +1,4 @@
 class Orc
   livecheck :url => "https://cgit.freedesktop.org/gstreamer/orc/",
-            :regex => %r{/gstreamer/orc/tag/\?id=orc-([0-9\.]+)}
+            :regex => %r{/gstreamer/orc/tag/\?h=orc-([0-9\.]+)}
 end
