03b15d2263afed7218dd7094b87d6958b3d6c09d
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:30 -0700

    gst-plugins-good: update livecheck

--- Livecheckables/gst-plugins-good.rb
+++ Livecheckables/gst-plugins-good.rb
@@ -1,4 +1,4 @@
 class GstPluginsGood
   livecheck :url => "https://gstreamer.freedesktop.org/src/gst-plugins-good/",
-            :regex => /href="gst-plugins-good-([0-9\.]+)\.t/
+            :regex => /href="gst-plugins-good-(1\.12\.[0-9\.]+)\.t/
 end
