96968bf77787d2012cc8e139fe5ca7a0dbbd88ca
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:28 -0700

    armadillo: update livecheck

--- Livecheckables/armadillo.rb
+++ Livecheckables/armadillo.rb
@@ -1,4 +1,4 @@
 class Armadillo
    livecheck :url => "http://bit.ly/2kLBPoS", # http://arma.sourceforge.net/download.html
-             :regex => /Stable Version.*armadillo-(\d+.\d+.\d+).tar.xz.*Old Versions/m
+             :regex => /Stable Version.*armadillo-(\d+.\d+.\d+).tar.xz.*Development Versions/m
 end
