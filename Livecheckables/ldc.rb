023953f4d6c8b54382dd7c7f99f45b31b36d4b24
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:31 -0700

    ldc: update livecheck

--- Livecheckables/ldc.rb
+++ Livecheckables/ldc.rb
@@ -1,4 +1,4 @@
 class Ldc
   livecheck :url => "https://github.com/ldc-developers/ldc/releases",
-            :regex => %r{href="/ldc-developers/ldc/tree/v?([0-9\.]+)}
+            :regex => %r{Latest.*?href="/ldc-developers/ldc/tree/v?([0-9\.]+)}m
 end
