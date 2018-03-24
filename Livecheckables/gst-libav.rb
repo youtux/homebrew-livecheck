7ca797be11b8ab81e5fd1125a11315b669eccfec
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:30 -0700

    gst-libav: update livecheck

--- Livecheckables/gst-libav.rb
+++ Livecheckables/gst-libav.rb
@@ -1,4 +1,4 @@
 class GstLibav
   livecheck :url => "https://gstreamer.freedesktop.org/src/gst-libav/",
-            :regex => /href="gst-libav-([0-9\.]+)\.t/
+            :regex => /href="gst-libav-(1\.12\.[0-9\.]+)\.t/
 end
