0e7ed2a0a796196692367238fa55b9385cb6809f
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:33 -0700

    zabbix: update livecheck

--- Livecheckables/zabbix.rb
+++ Livecheckables/zabbix.rb
@@ -1,4 +1,4 @@
 class Zabbix
-  livecheck :url => "http://www.zabbix.com/download",
-            :regex => %r{href="http://sourceforge\.net/projects/zabbix/files/ZABBIX%20Latest%20Stable/([0-9,\.]+)/zabbix-.*\.tar\.gz/download}
+  livecheck :url => "https://www.zabbix.com/download_sources",
+            :regex => %r{href="https://sourceforge\.net/projects/zabbix/files/ZABBIX%20Latest%20Stable/([0-9,\.]+)/zabbix-.*\.tar\.gz/download}
 end
