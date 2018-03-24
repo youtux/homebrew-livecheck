c5059ab77baf7cc996c93ac9d31995c1037aaa28
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:32 -0700

    open-mpi: update livecheck

--- Livecheckables/open-mpi.rb
+++ Livecheckables/open-mpi.rb
@@ -1,4 +1,4 @@
 class OpenMpi
-  livecheck :url => "https://www.open-mpi.org/software/ompi/v2.0/downloads",
-            :regex => /openmpi-(.*?)\.tar\.bz2/
+  livecheck :url => "https://www.open-mpi.org/software/ompi/v3.0/downloads",
+            :regex => /openmpi-([0-9\.]+)\.t/
 end
