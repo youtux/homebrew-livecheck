464aadf73a6a24df3b6fb42c05373bb7d95a781d
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:58:01 -0700

    libsoup: update livecheck

--- Livecheckables/libsoup.rb
+++ Livecheckables/libsoup.rb
@@ -1,4 +1,4 @@
 class Libsoup < Formula
   livecheck :url => "https://download.gnome.org/sources/libsoup/cache.json",
-            :regex => /libsoup-(2\.60\.[\d.]+)\.t/
+            :regex => /libsoup-(2\.62\.[\d.]+)\.t/
 end
