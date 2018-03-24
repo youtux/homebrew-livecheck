657ef2366f811cd464ca010f83cbbcdcbeef4864
ilovezfs <ilovezfs@icloud.com>
2018-03-23 23:07:30 -0700

    gsmartcontrol: update livecheck

--- Livecheckables/gsmartcontrol.rb
+++ Livecheckables/gsmartcontrol.rb
@@ -1,4 +1,4 @@
 class Gsmartcontrol
-  livecheck :url => "http://gsmartcontrol.sourceforge.net/",
+  livecheck :url => "https://gsmartcontrol.sourceforge.io/",
             :regex => %r{GSmartControl (\d+(\.\d+)*)}
 end
