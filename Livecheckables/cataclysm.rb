261be2cf5a2a2bdb85ac3f1f5ef63155d62610f7
ilovezfs <ilovezfs@icloud.com>
2018-05-03 07:27:55 -0700

    cataclysm: add livecheck

---
+++ Livecheckables/cataclysm.rb
@@ -0,0 +1,4 @@
+class Cataclysm
+  livecheck :url => "https://github.com/CleverRaven/Cataclysm-DDA/releases",
+            :regex => %r{Latest.*?href="/CleverRaven/Cataclysm-DDA/tree/v?([0-9a-zA-Z\.]+)}m
+end
