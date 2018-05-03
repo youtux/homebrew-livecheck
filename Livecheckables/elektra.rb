ac4bad3fd95ee411d76088bda4120a4f39388744
ilovezfs <ilovezfs@icloud.com>
2018-05-03 03:53:07 -0700

    elektra: add livecheck

---
+++ Livecheckables/elektra.rb
@@ -0,0 +1,4 @@
+class Elektra
+  livecheck :url => "https://github.com/ElektraInitiative/libelektra/releases",
+            :regex => %r{Latest.*?href="/ElektraInitiative/libelektra/tree/v?([0-9\.]+)}m
+end
