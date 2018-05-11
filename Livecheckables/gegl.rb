9c0e18b8f7ace82a67e780e3de495451494da3d8
ilovezfs <ilovezfs@icloud.com>
2018-05-11 16:54:43 -0700

    gegl: update livecheck

--- Livecheckables/gegl.rb
+++ Livecheckables/gegl.rb
@@ -1,4 +1,4 @@
 class Gegl
-  livecheck :url => "https://download.gimp.org/pub/gegl/0.3/",
+  livecheck :url => "https://download.gimp.org/pub/gegl/0.4/",
             :regex => %r{gegl-(\d+(?:\.\d+)*)\.tar}
 end
