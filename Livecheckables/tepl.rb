2a18620227b88e8c46d7fb12edc886565d18b07c
ilovezfs <ilovezfs@icloud.com>
2018-05-28 02:16:16 -0700

    tepl: update livecheck

--- Livecheckables/tepl.rb
+++ Livecheckables/tepl.rb
@@ -1,4 +1,4 @@
 class Tepl
-  livecheck :url => "https://download.gnome.org/sources/tepl/3.0/",
+  livecheck :url => "https://download.gnome.org/sources/tepl/4.0/",
             :regex => /href="LATEST-IS-([0-9\.]+)"/
 end
