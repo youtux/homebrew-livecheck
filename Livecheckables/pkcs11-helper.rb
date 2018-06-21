af5cd6834440d32dbd22e46c592ea2c8c09076d7
ilovezfs <ilovezfs@icloud.com>
2018-06-21 05:28:56 -0700

    pkcs11-helper: add livecheck

---
+++ Livecheckables/pkcs11-helper.rb
@@ -0,0 +1,4 @@
+class Pkcs11Helper
+  livecheck :url => "https://github.com/OpenSC/pkcs11-helper/releases",
+            :regex => %r{Latest.*?href="/OpenSC/pkcs11-helper/tree/pkcs11-helper-([0-9\.]+)}m
+end
