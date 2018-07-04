8d974cb4c90defc07f83fd366cf82b40c9fca5fa
commitay <commitay@users.noreply.github.com>
2018-07-04 20:26:42 +1000

    mongodb@3.6: update livecheck

--- Livecheckables/mongodb@3.6.rb
+++ Livecheckables/mongodb@3.6.rb
@@ -1,4 +1,4 @@
 class MongodbAT36
-  livecheck :url => "https://docs.mongodb.com/manual/release-notes/3.6-changelog/",
-            :regex => /(\d+.\d+.\d+) Changelog/
+  livecheck :url => "https://www.mongodb.org/downloads",
+            :regex => /mongodb-src-r(3\.6\.\d+)\./
 end
