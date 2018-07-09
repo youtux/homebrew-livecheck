88b8871f64020f244fd5a20e26b0734877773d30
ilovezfs <ilovezfs@icloud.com>
2018-07-08 23:07:43 -0700

    shairport-sync: update livecheck

--- Livecheckables/shairport-sync.rb
+++ Livecheckables/shairport-sync.rb
@@ -1,4 +1,4 @@
 class ShairportSync
-  livecheck :url => "https://github.com/mikebrady/shairport-sync/releases",
+  livecheck :url => "https://github.com/mikebrady/shairport-sync/releases/latest",
             :regex => %r{latest.*?href="/mikebrady/shairport-sync/tree/([0-9\.]+)"}m
 end
