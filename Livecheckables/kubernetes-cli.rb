38d4f3a5d2440e58fb1de5a5001251242e590162
ilovezfs <ilovezfs@icloud.com>
2018-07-08 23:07:43 -0700

    kubernetes-cli: update livecheck

--- Livecheckables/kubernetes-cli.rb
+++ Livecheckables/kubernetes-cli.rb
@@ -1,4 +1,4 @@
 class KubernetesCli
   livecheck :url => "https://github.com/kubernetes/kubernetes/releases",
-            :regex => %r{Latest.*?href="/kubernetes/kubernetes/tree/v?([0-9\.]+)}m
+            :regex => %r{href="/kubernetes/kubernetes/tree/v?(1\.11\.[0-9\.]+)}m
 end
