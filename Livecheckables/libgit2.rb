53b7f6764dc15455033d823d81e8c975ff5aa7f1
ilovezfs <ilovezfs@icloud.com>
2018-07-16 22:07:48 -0700

    libgit2: update livecheck

--- Livecheckables/libgit2.rb
+++ Livecheckables/libgit2.rb
@@ -1,4 +1,4 @@
 class Libgit2
   livecheck :url => "https://github.com/libgit2/libgit2/releases",
-            :regex => %r{Latest.*?href="/libgit2/libgit2/tree/v?([0-9\.]+)}m
+            :regex => %r{href="/libgit2/libgit2/tree/v?(0\.2[789]\.[0-9\.]+)}
 end
