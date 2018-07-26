ee2ecec31047d6e207fdf431e610d414cd72d9a2
commitay <commitay@users.noreply.github.com>
2018-07-26 10:06:57 +1000

    yarn: update livecheck

--- Livecheckables/yarn.rb
+++ Livecheckables/yarn.rb
@@ -1,4 +1,4 @@
 class Yarn
   livecheck :url => "https://yarnpkg.com/en/",
-            :regex => %r{<a href="https://github.com/yarnpkg/yarn/releases/tag/v([0-9,\.]+)">}
+            :regex => /Stable:.*?v([0-9\.]+)/m
 end
