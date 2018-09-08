ad6eb67537399f8721cec267d1e4beaf74e1103b
Tommy Yang <yt940928@gmail.com>
2018-09-08 16:52:46 -0700

    Generalize npm livechecks (#95)
    remove livechecks that are covered by npm euristic

--- Livecheckables/joplin.rb
+++
@@ -1,4 +0,0 @@
-class Joplin
-  livecheck :url => "https://www.npmjs.com/package/joplin",
-            :regex => %r{package__sidebarText.*?>([0-9\.]+)</p>}
-end
--- Livecheckables/quicktype.rb
+++
@@ -1,4 +0,0 @@
-class Quicktype
-  livecheck :url => "https://www.npmjs.com/package/quicktype",
-            :regex => %r{package__sidebarText.*?>([0-9\.]+)</p>}
-end
