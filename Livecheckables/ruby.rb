c124ab8942994cca7bf103f5e70e3bad7ade5016
commitay <commitay@users.noreply.github.com>
2018-10-20 21:19:10 +1000

    ruby: update livecheck

--- Livecheckables/ruby.rb
+++ Livecheckables/ruby.rb
@@ -1,4 +1,4 @@
 class Ruby
-  livecheck :url => "http://cache.ruby-lang.org/pub/ruby/",
-            :regex => /ruby-(.*?)\.tar\.gz/
+  livecheck :url => "https://www.ruby-lang.org/en/downloads/",
+            :regex => /The current stable version is ([0-9\.]+)\./
 end
