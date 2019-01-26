d04c8df2f347643c17fe4c58e55a1f052edb6176
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2019-01-26 09:33:14 -0500

    adwaita-icon-theme: add livecheck

---
+++ Livecheckables/adwaita-icon-theme.rb
@@ -0,0 +1,4 @@
+class AdwaitaIconTheme
+  livecheck :url => "https://download.gnome.org/sources/adwaita-icon-theme/cache.json",
+            :regex => /adwaita-icon-theme-([\d.]+\.[\d.]+\.[\d.]+)\.t/
+end
