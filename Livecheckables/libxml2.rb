e68854b0ffa9b17d0e82d54be33d256484551dd8
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2019-01-26 09:44:32 -0500

    libxml2: update livecheck

--- Livecheckables/libxml2.rb
+++ Livecheckables/libxml2.rb
@@ -1,4 +1,4 @@
 class Libxml2
-  livecheck :url => "http://xmlsoft.org/news.html",
-            :regex => %r{<h3>[v]*(\d+(?:\.\d+)*): .*</h3>}
+  livecheck :url => "http://xmlsoft.org/sources",
+    :regex => /href=".*libxml2-([\d.]+\.[\d.]+\.[\d.]+)\.t/
 end
