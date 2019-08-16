21b6e5121168a9646d461eac5781bfa316b5aec4
Maxim Belkin <maxim.belkin@gmail.com>
2019-08-16 16:06:08 -0500

    per-tap livecheckables

--- livecheck/extend/formulary.rb
+++ livecheck/extend/formulary.rb
@@ -3,14 +3,23 @@ require_relative "formula"
 module Formulary
   class << self
     # extended to load the Livecheckable version of a formula
+
+     def livecheckable_path(path)
+       if (opt1 = Pathname.new(path).realpath.dirname.dirname / "Livecheckables" / path.basename).exist?
+         opt1
+       elsif (opt2 = Pathname.new(__FILE__).realpath.dirname / "../../Livecheckables" / path.basename ).exist?
+         opt2
+       end
+     end
+
     def load_formula(name, path, contents, namespace)
       mod = Module.new
       const_set(namespace, mod)
       mod.module_eval(contents, path)
 
-      livecheckable_path = LIVECHECKABLES_PATH / path.basename
+      livecheckable_path = livecheckable_path(path)
       if File.exist?(livecheckable_path)
-        puts "Loading #{LIVECHECKABLES_PATH / path.basename}" if ARGV.debug?
+        puts "Loading #{livecheckable_path}" if ARGV.debug?
         mod.module_eval(livecheckable_path.read, livecheckable_path)
       end
 
