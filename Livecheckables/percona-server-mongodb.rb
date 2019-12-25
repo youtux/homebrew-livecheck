9ce5d06b2be9f145cb4796979fddf2b6f3ee68d3
Thierry Moisan <thierry.moisan@gmail.com>
2019-12-25 10:40:24 -0500

    percona-server-mongodb: delete livecheckable

--- Livecheckables/percona-server-mongodb.rb
+++
@@ -1,4 +0,0 @@
-class PerconaServerMongodb
-  livecheck :url => "https://www.percona.com/downloads/percona-server-mongodb-LATEST/",
-            :regex => %r{value="percona-server-mongodb-LATEST/percona-server-mongodb-([0-9\-\.]+)"}
-end
