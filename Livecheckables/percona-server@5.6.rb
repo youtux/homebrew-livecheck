087ed4a8de8c3103453404af96e2b1a1e5159021
Thierry Moisan <thierry.moisan@gmail.com>
2019-12-30 14:27:39 -0500

    percona-server@5.6: delete livecheckable

--- Livecheckables/percona-server@5.6.rb
+++
@@ -1,4 +0,0 @@
-class PerconaServerAT56
-  livecheck :url => "https://www.percona.com/downloads/Percona-Server-5.6/LATEST/",
-            :regex => %r{value="Percona-Server-5.6/Percona-Server-([0-9\-\.]+)"}
-end
