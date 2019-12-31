f2b39b6f827bc1ac4bfedcc18fac13cdb208bfc5
Thierry Moisan <thierry.moisan@gmail.com>
2019-12-31 10:42:46 -0500

    cfr-decompiler: update regex

--- Livecheckables/cfr-decompiler.rb
+++ Livecheckables/cfr-decompiler.rb
@@ -1,4 +1,4 @@
 class CfrDecompiler
   livecheck :url => "http://www.benf.org/other/cfr/",
-            :regex => /href="cfr_([0-9\_\.]+)\.j/
+            :regex => /href=".*cfr-([0-9\_\.]+)\.j/
 end
