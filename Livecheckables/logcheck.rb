9a974ed68c38f41d266b0db693dec6b81a9707da
Thierry Moisan <thierry.moisan@gmail.com>
2020-02-02 15:28:03 -0500

    logcheck: fix livecheckable regex

--- Livecheckables/logcheck.rb
+++ Livecheckables/logcheck.rb
@@ -1,4 +1,4 @@
 class Logcheck
   livecheck :url => "https://packages.debian.org/unstable/logcheck",
-            :regex => %r{href="http://http.debian.net/debian/pool/main/l/logcheck/logcheck_([0-9,\.]+)\.tar}
+            :regex => %r{logcheck_([0-9,\.]+)\.tar}
 end
