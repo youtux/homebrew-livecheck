49baa5ce4ab83748a9ddad37dc1f2ed80ad1fcad
Thierry Moisan <thierry.moisan@gmail.com>
2020-02-10 09:25:36 -0500

    yaws: update livecheckable

--- Livecheckables/yaws.rb
+++ Livecheckables/yaws.rb
@@ -1,4 +1,4 @@
 class Yaws
-  livecheck :url => "http://yaws.hyber.org/download/",
-            :regex => /href="yaws-([0-9\.]+)\.t/
+  livecheck :url => "https://github.com/klacke/yaws/releases",
+            :regex => /href=".*yaws-([0-9\.]+)\.t/
 end
