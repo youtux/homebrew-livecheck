53aae1893fda7d12aadd6404d3f8a3a26127cbfd
Sam Ford <1584702+samford@users.noreply.github.com>
2020-02-28 22:14:49 -0500

    opencv: remove url, update regex

--- Livecheckables/opencv.rb
+++ Livecheckables/opencv.rb
@@ -1,4 +1,3 @@
 class Opencv
-  livecheck :url => "https://github.com/opencv/opencv/releases",
-            :regex => %r{Latest release.*?href="/opencv/opencv/tree/(3[0-9\.]+)"}m
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
