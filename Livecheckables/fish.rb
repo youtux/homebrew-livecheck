7339a839efddb11d7ab5afc2b82aebda85104392
Sam Ford <1584702+samford@users.noreply.github.com>
2020-02-28 22:33:59 -0500

    fish: remove url, update regex

--- Livecheckables/fish.rb
+++ Livecheckables/fish.rb
@@ -1,4 +1,3 @@
 class Fish
-  livecheck :url => "https://fishshell.com/release_notes.html",
-            :regex => /Release Notes for fish ([0-9\.]+)/
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
