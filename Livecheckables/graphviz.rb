234bc5a47497c6fc790726c8ff688b8e9c3fbc5e
Sam Ford <1584702+samford@users.noreply.github.com>
2020-02-29 15:21:14 -0500

    graphviz: remove url, update regex

--- Livecheckables/graphviz.rb
+++ Livecheckables/graphviz.rb
@@ -1,4 +1,3 @@
 class Graphviz
-  livecheck :url => "https://graphviz.gitlab.io/_pages/Download/Download_source.html",
-    :regex => /href=".*stable.*graphviz-([\d.]+\.[\d.]+\.[\d.]+)\.t/
+  livecheck :regex => /stable_release_(\d+(?:\.\d+)+)$/
 end
