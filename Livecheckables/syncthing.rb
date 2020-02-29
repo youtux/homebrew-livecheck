799f88b58a80b143054a7a3c8c3886ab63b1ecbc
Sam Ford <1584702+samford@users.noreply.github.com>
2020-02-29 14:34:21 -0500

    syncthing: remove url, update regex

--- Livecheckables/syncthing.rb
+++ Livecheckables/syncthing.rb
@@ -1,4 +1,3 @@
 class Syncthing
-  livecheck :url => "https://github.com/syncthing/syncthing/releases",
-            :regex => %r{href="/syncthing/syncthing/releases/download/.*syncthing-source-v([0-9,\.]+)\.tar}
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
