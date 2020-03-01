74db7a1e6c3f3efbe5c0bd75f3c8c15f94ad73b8
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-01 11:55:36 -0500

    utils: explicitly use URI.open

--- livecheck/utils.rb
+++ livecheck/utils.rb
@@ -22,7 +22,7 @@ end
 
 def page_matches(url, regex)
   puts %Q[Using page_match("#{url}", "#{regex}")] if Homebrew.args.debug?
-  page = open(url).read
+  page = URI.open(url).read
   matches = page.scan(regex)
   puts matches.join(", ") if Homebrew.args.debug?
   matches.map(&:first).uniq
