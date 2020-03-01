1438b34a19a67070a53d9d474462d1c97abba2ad
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-01 14:47:50 -0500

    osrm-backend: remove url, update regex

--- Livecheckables/osrm-backend.rb
+++ Livecheckables/osrm-backend.rb
@@ -1,5 +1,3 @@
 class OsrmBackend
-  livecheck :url => "https://api.github.com/repos/Project-OSRM/osrm-backend/releases/latest",
-            :regex => /([0-9\.]+\.[0-9\.]+)"/
-            # :regex => /"tag_name": "v?([0-9\.]+)",/
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
