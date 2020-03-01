1bf8cc210414f65c6841609091dd06bd0c2b4398
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-01 14:33:27 -0500

    debian-utils: remove unnecessary comments

--- Livecheckables/debianutils.rb
+++ Livecheckables/debianutils.rb
@@ -1,6 +1,4 @@
 class Debianutils
-#livecheck.type  regex
-#livecheck.regex ${name}_(\\d+(?:.\\d+)+).dsc
   livecheck :url => "https://packages.qa.debian.org/d/debianutils.html",
             :regex => /debianutils_(\d+(?:.\d+)+).dsc/
 end
