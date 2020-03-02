6b651144c24c7478eb0fd93ef208daa0aa69cb70
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-01 20:43:34 -0500

    cataclysm: remove url, update regex

--- Livecheckables/cataclysm.rb
+++ Livecheckables/cataclysm.rb
@@ -1,4 +1,3 @@
 class Cataclysm
-  livecheck :url   => "https://github.com/CleverRaven/Cataclysm-DDA/releases",
-            :regex => %r{Latest.*?href="/CleverRaven/Cataclysm-DDA/tree/v?([0-9a-zA-Z\.]+)}m
+  livecheck :regex => /^(\d+(\.(?:\d+|[A-Z]+))+)$/
 end
