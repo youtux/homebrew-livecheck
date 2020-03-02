473ec07fe91fa4055ff8cafef310dc229aaedc0e
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-01 21:19:20 -0500

    deployer: update regex

--- Livecheckables/deployer.rb
+++ Livecheckables/deployer.rb
@@ -1,4 +1,4 @@
 class Deployer
   livecheck :url   => "https://deployer.org/download",
-            :regex => %r{Latest version.*?href=".*?/v?([0-9,\.]+)/deployer\.phar}m
+            :regex => %r{/releases/v?(\d+(?:\.\d+)+)/deployer.phar}
 end
