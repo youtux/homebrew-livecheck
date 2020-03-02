97b1146e724e7f1843c5c4b86d056a289b4b8dee
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-02 00:17:47 -0500

    dnscrypt-proxy: remove url, update regex

--- Livecheckables/dnscrypt-proxy.rb
+++ Livecheckables/dnscrypt-proxy.rb
@@ -1,4 +1,3 @@
 class DnscryptProxy
-  livecheck :url   => "https://github.com/jedisct1/dnscrypt-proxy/releases",
-            :regex => %r{Latest.*?href="/jedisct1/dnscrypt-proxy/tree/v?([0-9\.]+)}m
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
