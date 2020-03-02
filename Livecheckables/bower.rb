0cca72c282cd26f4694540d23762f7164fd36b3d
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-01 20:09:09 -0500

    bower: remove url, update regex

--- Livecheckables/bower.rb
+++ Livecheckables/bower.rb
@@ -1,4 +1,3 @@
 class Bower
-  livecheck :url   => "https://www.npmjs.com/package/bower",
-            :regex => %r{package__sidebarText.*?>([0-9\.]+)</p>}
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
