be398e7f93088fe97394d5b0c62187fafa615bb4
Rui Chen <chenrui333@gmail.com>
2020-03-08 14:34:35 -0400

    tomcat: update regex

--- Livecheckables/tomcat@7.rb
+++ Livecheckables/tomcat@7.rb
@@ -1,4 +1,4 @@
 class TomcatAT7
   livecheck :url   => "https://archive.apache.org/dist/tomcat/tomcat-7/",
-            :regex => %r{href="v(.*?)/"}
+            :regex => %r{href="v?(\d+(?:\.\d+)+)/"}
 end
--- Livecheckables/tomcat@8.rb
+++ Livecheckables/tomcat@8.rb
@@ -1,4 +1,4 @@
 class TomcatAT8
   livecheck :url   => "https://archive.apache.org/dist/tomcat/tomcat-8/",
-            :regex => %r{href="v(.*?)/"}
+            :regex => %r{href="v?(\d+(?:\.\d+)+)/"}
 end
