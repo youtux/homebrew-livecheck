5e8db075c5c18a0ea671c15ff997e60c702143e5
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-08 19:25:43 -0400

    openshift-cli: remove url and update regex

--- Livecheckables/openshift-cli.rb
+++ Livecheckables/openshift-cli.rb
@@ -1,4 +1,3 @@
 class OpenshiftCli
-  livecheck :url   => "https://github.com/openshift/origin/releases",
-            :regex => %r{Latest release.*?href="/openshift/origin/tree/v?([0-9\.]+)"}m
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
