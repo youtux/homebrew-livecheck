2c24a9a9781a3a88c5347abffb54918343b8e23c
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-07 21:47:24 -0500

    freeswitch: remove url and update regex

--- Livecheckables/freeswitch.rb
+++ Livecheckables/freeswitch.rb
@@ -1,4 +1,3 @@
 class Freeswitch
-  livecheck :url   => "https://freeswitch.org/confluence/display/FREESWITCH/macOS+Installation",
-            :regex => /<p>([0-9\.]+) .*?Current Production/
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
