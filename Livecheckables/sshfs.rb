7cbe649985cd526ce0db8f9656cd37ccc8744b7c
Rui Chen <chenrui333@gmail.com>
2020-03-08 22:58:44 -0400

    sshfs: update url and regex

--- Livecheckables/sshfs.rb
+++ Livecheckables/sshfs.rb
@@ -1,4 +1,3 @@
 class Sshfs
-  livecheck :url   => "https://github.com/libfuse/sshfs/releases",
-            :regex => %r{href="/libfuse/sshfs/tree/sshfs-(2\.[0-9\.]+)"}
+  livecheck :regex => /^sshfs-(\d+(?:\.\d+)+)$/
 end
