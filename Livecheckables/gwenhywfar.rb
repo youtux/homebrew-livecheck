b4c90638924f18d1e3c7aaca1ec4667be6aa6a6c
chenrui <chenrui333@gmail.com>
2020-03-08 22:40:17 -0400

    gwenhywfar: restrict href
    Co-Authored-By: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/gwenhywfar.rb
+++ Livecheckables/gwenhywfar.rb
@@ -1,4 +1,4 @@
 class Gwenhywfar
   livecheck :url   => "https://www.aquamaniac.de/rdm/projects/gwenhywfar/files",
-            :regex => /gwenhywfar-(\d+(?:\.\d+)+)\.t/
+            :regex => %r{href="[^"]+/gwenhywfar-(\d+(?:\.\d+)+)\.t}
 end
