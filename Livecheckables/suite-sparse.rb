4ac7ac12d6f01c8b4066733984a7b6ce211617c4
Rui Chen <chenrui333@gmail.com>
2020-03-08 22:56:01 -0400

    suite-sparse: update url and regex

--- Livecheckables/suite-sparse.rb
+++ Livecheckables/suite-sparse.rb
@@ -1,4 +1,3 @@
 class SuiteSparse
-  livecheck :url   => "http://faculty.cse.tamu.edu/davis/suitesparse.html",
-            :regex => %r{href=".*?/SuiteSparse-([0-9\.]+)\.t}
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
