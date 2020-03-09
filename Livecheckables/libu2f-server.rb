10218ea5bed24cc62475d42a6f2f92503f5cad0d
Rui Chen <chenrui333@gmail.com>
2020-03-09 00:46:10 -0400

    libu2f-server: update url and regex

--- Livecheckables/libu2f-server.rb
+++ Livecheckables/libu2f-server.rb
@@ -1,4 +1,4 @@
 class Libu2fServer
-  livecheck :url   => "https://developers.yubico.com/libu2f-server/Releases",
-            :regex => /href="libu2f-server-([0-9\.]+)\.t/
+  livecheck :url   => "https://developers.yubico.com/libu2f-server/Releases/",
+            :regex => /href="libu2f-server-(\d+(?:\.\d+)+)\.t/
 end
