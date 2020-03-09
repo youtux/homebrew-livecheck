79f6de1c729892a1c42fca341afa9e575917b0db
Rui Chen <chenrui333@gmail.com>
2020-03-09 01:56:15 -0400

    gcc: update url and regex

--- Livecheckables/gcc.rb
+++ Livecheckables/gcc.rb
@@ -1,4 +1,4 @@
 class Gcc
-  livecheck :url   => "https://gcc.gnu.org/git/gcc.git",
-            :regex => %r{^releases/gcc-([\d\.]+)$}
+  livecheck :url   => "https://ftp.gnu.org/gnu/gcc/?C=M&O=D",
+            :regex => %r{href="gcc-(\d+(?:\.\d+)+)(?:/?"|\.t)}
 end
