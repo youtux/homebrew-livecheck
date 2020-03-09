9b5640bb5fd086693f1fdc20779eb9e7da499a28
Rui Chen <chenrui333@gmail.com>
2020-03-08 21:38:08 -0400

    gwenhywfar: update url and regex

--- Livecheckables/gwenhywfar.rb
+++ Livecheckables/gwenhywfar.rb
@@ -1,4 +1,4 @@
 class Gwenhywfar
-  livecheck :url   => "https://www.aquamaniac.de/sites/download/packages.php",
-            :regex => /gwenhywfar-([0-9\.]+)\.t/
+  livecheck :url   => "https://www.aquamaniac.de/rdm/projects/gwenhywfar/files",
+            :regex => /gwenhywfar-(\d+(?:\.\d+)+)\.t/
 end
