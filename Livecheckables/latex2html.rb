c65d746f29355c22f05f059e17df81d5032fd407
Rui Chen <chenrui333@gmail.com>
2020-03-09 01:06:48 -0400

    latex2html: update url and regex

--- Livecheckables/latex2html.rb
+++ Livecheckables/latex2html.rb
@@ -1,4 +1,3 @@
 class Latex2html
-  livecheck :url   => "http://mirrors.ctan.org/support/latex2html/",
-            :regex => /href="latex2html-([0-9\.]+)\.t/
+  livecheck :regex => /^v?(\d+(?:\.\d+)*)$/
 end
