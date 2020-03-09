83c26642593262866f18f503b09711c314efcef5
Rui Chen <chenrui333@gmail.com>
2020-03-09 00:21:04 -0400

    scamper: fix brew style

--- Livecheckables/scamper.rb
+++ Livecheckables/scamper.rb
@@ -1,4 +1,4 @@
 class Scamper
   livecheck :url   => "https://www.caida.org/tools/measurement/scamper/code/?C=M&O=D",
-            :regex => %r{href="scamper-cvs-([0-9\.]+)\.t}
+            :regex => /href="scamper-cvs-([0-9\.]+)\.t/
 end
