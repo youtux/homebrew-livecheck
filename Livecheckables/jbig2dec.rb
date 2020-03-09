f7793442a460bae40b7e4e9c98a04dc65278ad29
Rui Chen <chenrui333@gmail.com>
2020-03-09 01:23:06 -0400

    jbig2dec: update url and regex

--- Livecheckables/jbig2dec.rb
+++ Livecheckables/jbig2dec.rb
@@ -1,4 +1,4 @@
 class Jbig2dec
-  livecheck :url   => "https://artifex.com/developers-ghostscript-jbig2dec/",
-            :regex => %r{href=.*?/jbig2dec-([0-9\.]+)\.t}
+  livecheck :url   => "https://jbig2dec.com",
+            :regex => /href="[^"]*?jbig2dec-v?(\d+(?:\.\d+)+)\.t/
 end
