00e1d3fab8fbf5fce1ba7016c9ec05cf5aaf5517
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-08 21:24:32 -0400

    atool: update url and regex

--- Livecheckables/atool.rb
+++ Livecheckables/atool.rb
@@ -1,4 +1,4 @@
 class Atool
-  livecheck :url   => "https://namesdir.com/mirrors/nongnu/atool/",
-            :regex => /atool-(\d+(?:\.\d+)+)/
+  livecheck :url   => "https://namesdir.com/mirrors/nongnu/atool/?C=M&O=D",
+            :regex => /href="atool-(\d+(?:\.\d+)+)\.t/
 end
