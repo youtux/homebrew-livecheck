9b0381af49afd7c8c0f711dfff285eeea5af5ace
Rui Chen <chenrui333@gmail.com>
2020-03-09 01:59:17 -0400

    mkvtoolnix: update regex

--- Livecheckables/mkvtoolnix.rb
+++ Livecheckables/mkvtoolnix.rb
@@ -1,4 +1,4 @@
 class Mkvtoolnix
   livecheck :url   => "https://mkvtoolnix.download/sources/",
-            :regex => /href="mkvtoolnix-([0-9\.]+)\.t/
+            :regex => /mkvtoolnix-([0-9\.]+)\.t/
 end
