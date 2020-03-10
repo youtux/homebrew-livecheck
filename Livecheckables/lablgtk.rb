fd964c91dc53ee2212f752543c479ffb16c257c1
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-10 19:44:46 -0400

    lablgtk: remove url and update regex

--- Livecheckables/lablgtk.rb
+++ Livecheckables/lablgtk.rb
@@ -1,4 +1,3 @@
 class Lablgtk
-  livecheck :url   => "http://lablgtk.forge.ocamlcore.org",
-            :regex => %r{href=".*?/lablgtk-([0-9\.]+)\.t}
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
