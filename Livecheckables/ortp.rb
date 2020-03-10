1eee774c5d7c64c31696b7013f5ed3620d7bbcd1
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-10 18:33:26 -0400

    ortp: update regex

--- Livecheckables/ortp.rb
+++ Livecheckables/ortp.rb
@@ -1,4 +1,4 @@
 class Ortp
   livecheck :url   => "https://www.linphone.org/releases/sources/ortp",
-            :regex => /href="ortp-([0-9\.]+)\.t/
+            :regex => /href="ortp-(\d+(?:\.\d+)+)\.t/
 end
