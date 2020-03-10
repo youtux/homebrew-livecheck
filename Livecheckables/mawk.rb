45794be7a978a46c6a6cd9cc242e0bf5e0bb3e76
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-10 17:17:56 -0400

    mawk: update url and regex

--- Livecheckables/mawk.rb
+++ Livecheckables/mawk.rb
@@ -1,4 +1,4 @@
 class Mawk
-  livecheck :url   => "https://invisible-mirror.net/archives/mawk/",
-            :regex => /href="mawk-(.*)\.t.*?"/
+  livecheck :url   => "https://invisible-mirror.net/archives/mawk/?C=M&O=D",
+            :regex => /href="mawk-(\d+(?:\.\d+)+(?:-\d+)?)\.t/
 end
