8c0b92cb91e81003151b2b1189a2b2144d6b11d3
Rui Chen <chenrui333@gmail.com>
2020-03-09 23:28:56 -0400

    digdag: update regex

--- Livecheckables/digdag.rb
+++ Livecheckables/digdag.rb
@@ -1,3 +1,4 @@
 class Digdag
-  livecheck :url => "https://github.com/treasure-data/digdag.git"
+  livecheck :url   => "https://github.com/treasure-data/digdag.git",
+            :regex => /^v?(\d+(?:\.\d+)+)$/
 end
