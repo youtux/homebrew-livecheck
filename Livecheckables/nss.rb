53f921ee1fb6da7a9f0071a92e55e558c5e317e9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-10 14:37:47 -0400

    nss: update regex

--- Livecheckables/nss.rb
+++ Livecheckables/nss.rb
@@ -1,4 +1,4 @@
 class Nss
   livecheck :url   => "https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS/NSS_Releases",
-            :regex => %r{href="/en-US/docs/Mozilla/Projects/NSS/NSS_([0-9\.]+)_release_notes"}
+            :regex => /href="[^"]*?NSS_(\d+(?:\.\d+)+)_release_notes"/
 end
