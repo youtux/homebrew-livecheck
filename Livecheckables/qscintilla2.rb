a2ee1e59260225d8bb7b15c0399817eb22b128f7
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-10 15:07:36 -0400

    qscintilla2: update regex

--- Livecheckables/qscintilla2.rb
+++ Livecheckables/qscintilla2.rb
@@ -1,4 +1,4 @@
 class Qscintilla2
   livecheck :url   => "https://www.riverbankcomputing.com/software/qscintilla/download",
-            :regex => /QScintilla[_-]gpl-([0-9\.]+)\.t/
+            :regex => /href="[^"]*?QScintilla[^"]*?(\d+(?:\.\d+)+)\.t/i
 end
