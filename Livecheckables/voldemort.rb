e8c1baf128f830a71dfce69abb051e28205d1ac0
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-10 20:03:27 -0400

    voldemort: remove url and update regex

--- Livecheckables/voldemort.rb
+++ Livecheckables/voldemort.rb
@@ -1,4 +1,3 @@
 class Voldemort
-  livecheck :url   => "https://github.com/voldemort/voldemort/releases",
-            :regex => %r{href="/voldemort/voldemort/tree/release-([0-9\.]+)-cutoff}
+  livecheck :regex => /(?:release-)?v?(\d+(?:\.\d+)+)(?:-cutoff)?/
 end
