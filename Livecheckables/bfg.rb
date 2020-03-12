fdd6855500466dd2705592bdcb6faca20690342c
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-11 21:16:56 -0400

    bfg: update url and regex

--- Livecheckables/bfg.rb
+++ Livecheckables/bfg.rb
@@ -1,4 +1,4 @@
 class Bfg
-  livecheck :url   => "https://github.com/rtyley/bfg-repo-cleaner",
-            :regex => /^(v.*)/
+  livecheck :url   => "https://github.com/rtyley/bfg-repo-cleaner.git",
+            :regex => /^v?(\d+(?:\.\d+)+)$/
 end
