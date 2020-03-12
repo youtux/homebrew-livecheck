a175723d418a6e991a3aaf7267e5f1838e834161
EricFromCanada <enk3@outlook.com>
2020-03-12 00:55:16 -0400

    delete bitbucket.org livecheckables

--- Livecheckables/ompl.rb
+++
@@ -1,4 +0,0 @@
-class Ompl
-  livecheck :url   => "https://bitbucket.org/ompl/ompl/downloads/",
-            :regex => %r{href=.*?/ompl-([0-9\.]+)-Source\.t}
-end
--- Livecheckables/sagittarius-scheme.rb
+++
@@ -1,4 +0,0 @@
-class SagittariusScheme
-  livecheck :url   => "https://bitbucket.org/ktakashi/sagittarius-scheme/downloads/",
-            :regex => %r{href=".*?/sagittarius-([0-9\.]+)\.t}
-end
