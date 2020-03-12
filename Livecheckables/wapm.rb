4b54b021e5d5c8177e06149bc40263a88ba5f6b6
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-11 21:50:47 -0400

    wapm: update regex and add explanatory comment

--- Livecheckables/wapm.rb
+++ Livecheckables/wapm.rb
@@ -1,3 +1,5 @@
 class Wapm
-  livecheck :regex => /^v?[^3]([\d\.]+)/
+  # There is a "3.2" tag in the Git repo that we avoid by only matching versions
+  # with 3+ parts (e.g., 0.1.2)
+  livecheck :regex => /^v?(\d+(?:\.\d+){2,})/
 end
