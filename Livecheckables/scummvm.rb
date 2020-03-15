b69182c2839f511b6dd5189b272b7f5a82167298
Matt Keller <von.helmut@gmail.com>
2020-03-15 15:53:23 -0400

    scummvm: update url and regex

--- Livecheckables/scummvm.rb
+++ Livecheckables/scummvm.rb
@@ -1,4 +1,4 @@
 class Scummvm
-  livecheck :url   => "https://www.scummvm.org/downloads/",
-            :regex => %r{href=".*?/scummvm-([0-9\.]+)\.t}
+  livecheck :url   => "https://github.com/scummvm/scummvm.git",
+            :regex => /^v?(\d+(?:\.\d+)+)$/
 end
