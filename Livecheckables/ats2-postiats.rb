2dd2fa6d231166671d4b2d4f84964111370a04e1
Thierry Moisan <thierry.moisan@gmail.com>
2020-03-15 09:07:08 -0400

    ats2-postiats: simplify regex

--- Livecheckables/ats2-postiats.rb
+++ Livecheckables/ats2-postiats.rb
@@ -1,4 +1,3 @@
 class Ats2Postiats
-  livecheck :url   => "http://www.ats-lang.org/Downloads.html",
-            :regex => %r{The current <em>stable</em> release of ATS2 is at\n<a href="http://sourceforge.net/projects/ats2-lang/download">ATS2-([0-9\.]+)</a>}
+  livecheck :regex => /ATS2-Postiats-v?(\d+(?:\.\d+)+)\.t/i
 end
