71c2c6e160229b42ddaa2cb484fbe6b1a9be3260
Rui Chen <chenrui333@gmail.com>
2020-03-21 18:10:02 -0400

    gtksourceviewmm3: add livecheckable

---
+++ Livecheckables/gtksourceviewmm3.rb
@@ -0,0 +1,4 @@
+class Gtksourceviewmm3
+  livecheck :url   => "https://download.gnome.org/sources/gtksourceviewmm/",
+            :regex => /gtksourceviewmm-(3\.([0-8]\d*?)?[02468](?:\.\d+)+)\.t/
+end
