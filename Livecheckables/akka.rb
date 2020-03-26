4d7debb3db1a3bc5c0c14ba8a7bc14a8fcca0cdc
chenrui <rui@meetup.com>
2020-03-26 12:16:54 -0400

    Update Livecheckables/akka.rb
    Co-Authored-By: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/akka.rb
+++ Livecheckables/akka.rb
@@ -2,5 +2,5 @@ class Akka
   livecheck :url   => "https://github.com/akka/akka.git",
             # single digit for major version since
             # v2.6.4 is newer than v10.1.0
-            :regex => /^v?((\d){1}(?:\.\d+)+)$/
+            :regex => /^v?(\d(?:\.\d+)+)$/
 end
