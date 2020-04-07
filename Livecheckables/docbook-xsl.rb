26f361f8847fc6b2b20c3571de39b7434a92a4ad
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-07 15:25:54 -0400

    docbook-xsl: update regex

--- Livecheckables/docbook-xsl.rb
+++ Livecheckables/docbook-xsl.rb
@@ -1,4 +1,4 @@
 class DocbookXsl
   livecheck :url   => "https://github.com/docbook/xslt10-stylesheets.git",
-            :regex => %r{^release\/(\d+(?:\.\d+)+)$}i
+            :regex => %r{^(?:release/)?(\d+(?:\.\d+)+)$}i
 end
