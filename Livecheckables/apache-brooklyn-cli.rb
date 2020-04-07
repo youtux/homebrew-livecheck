c5f1bd773f97e8271e8bdf5950735a446a5a2460
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-07 14:38:41 -0400

    apache-brooklyn-cli: update regex

--- Livecheckables/apache-brooklyn-cli.rb
+++ Livecheckables/apache-brooklyn-cli.rb
@@ -1,4 +1,4 @@
 class ApacheBrooklynCli
   livecheck :url   => "https://github.com/apache/brooklyn-client.git",
-            :regex => %r{^rel\/apache-brooklyn-?(\d+(?:\.\d+)+)$}i
+            :regex => %r{^(?:rel/)?apache-brooklyn-(\d+(?:\.\d+)+)$}i
 end
