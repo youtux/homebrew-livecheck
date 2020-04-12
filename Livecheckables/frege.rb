e669079cc4a1282c0698d35741cc76d351166c64
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-12 14:30:42 -0400

    frege: update url and regex

--- Livecheckables/frege.rb
+++ Livecheckables/frege.rb
@@ -1,4 +1,4 @@
 class Frege
-  livecheck :url   => "https://github.com/Frege/frege/releases",
-            :regex => %r{public\/frege([0-9\.]+)}i
+  livecheck :url   => "https://github.com/Frege/frege/releases/latest",
+            :regex => /href=.+?frege(\d+(?:\.\d+)+)\.jar/
 end
