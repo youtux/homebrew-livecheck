ffbdb7cf97dfd1da9fd4dd3a9fc9c7e6518c7607
chenrui <chenrui333@gmail.com>
2020-04-12 01:41:25 -0400

    nuget: only use recommended latest version (#519)

--- Livecheckables/nuget.rb
+++ Livecheckables/nuget.rb
@@ -1,4 +1,4 @@
 class Nuget
   livecheck :url   => "https://dist.nuget.org/index.json",
-            :regex => /"displayName":\s*?"nuget.exe",\s*?"version":\s*?"v?(\d+(?:\.\d+)+)"/i
+            :regex => /"displayName":\s*?"nuget.exe - recommended latest",\s*?"version":\s*?"v?(\d+(?:\.\d+)+)"/i
 end
