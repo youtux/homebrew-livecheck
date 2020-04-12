d95b366b2a9eada95372126a22abf12ae1c934bd
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-12 13:43:38 -0400

    geoserver: update url and regex

--- Livecheckables/geoserver.rb
+++ Livecheckables/geoserver.rb
@@ -1,6 +1,8 @@
 class Geoserver
-  livecheck :url   => "https://sourceforge.net/projects/geoserver/",
-            # exclude 2.17-RC
-            # also rss feeds does not have 2.16.2-bin.zip
-            :regex => /geoserver-(\d+(?:\.\d+)+)-[^RC]/
+  # GeoServer releases contain a large number of files for each version, so the
+  # SourceForge RSS feed may only contain the latest version (which may have a
+  # different major/minor version than the latest stable). We check the GitHub
+  # repo tags here instead, to make sure we get all the latest versions.
+  livecheck :url   => "https://github.com/geoserver/geoserver",
+            :regex => /^v?(\d+(?:\.\d+)+)$/
 end
