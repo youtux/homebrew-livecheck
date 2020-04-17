804d4ce6b1cce201f230d89b0f8247db85254ba5
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-16 22:21:58 -0400

    gtksourceview4: update regex

--- Livecheckables/gtksourceview4.rb
+++ Livecheckables/gtksourceview4.rb
@@ -1,4 +1,4 @@
 class Gtksourceview4
   livecheck :url   => "https://download.gnome.org/sources/gtksourceview/",
-            :regex => /gtksourceview-(4\.([0-8]\d*?)?[02468](?:\.\d+)+)\.t/
+            :regex => /gtksourceview-(4\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/
 end
