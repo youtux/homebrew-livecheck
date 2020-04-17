3e5e10d7b2e5f7a9dc962af554c73048541cbbda
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-16 22:20:47 -0400

    gtksourceview3: update regex

--- Livecheckables/gtksourceview3.rb
+++ Livecheckables/gtksourceview3.rb
@@ -1,4 +1,4 @@
 class Gtksourceview3
   livecheck :url   => "https://download.gnome.org/sources/gtksourceview/",
-            :regex => /gtksourceview-(3\.([0-8]\d*?)?[02468](?:\.\d+)+)\.t/
+            :regex => /gtksourceview-(3\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/
 end
