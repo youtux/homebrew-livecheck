b5aa51ef95948eceeaba265517bc71f2b048c711
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-16 21:51:45 -0400

    gtksourceviewmm3: update regex

--- Livecheckables/gtksourceviewmm3.rb
+++ Livecheckables/gtksourceviewmm3.rb
@@ -1,4 +1,4 @@
 class Gtksourceviewmm3
   livecheck :url   => "https://download.gnome.org/sources/gtksourceviewmm/",
-            :regex => /gtksourceviewmm-(3\.([0-8]\d*?)?[02468](?:\.\d+)+)\.t/
+            :regex => /gtksourceviewmm-(3\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/
 end
