3faf8f80667a39ae7c2018437ce9fee430fbd919
Sam Ford <1584702+samford@users.noreply.github.com>
2020-04-16 22:52:36 -0400

    jdnssec-tools: update url and regex

--- Livecheckables/jdnssec-tools.rb
+++ Livecheckables/jdnssec-tools.rb
@@ -1,4 +1,4 @@
 class JdnssecTools
-  livecheck :url   => "https://github.com/dblacka/jdnssec-tools/releases",
-            :regex => %r{href=".*jdnssec-tools-(\d+(?:\.\d+)+)}i
+  livecheck :url   => "https://github.com/dblacka/jdnssec-tools.git",
+            :regex => /^v?(\d+(?:\.\d+)+)$/
 end
