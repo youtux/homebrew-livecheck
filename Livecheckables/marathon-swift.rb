6e2cc0e68e3796e32b8014252601c9e15ffdb09e
chenrui <chenrui333@gmail.com>
2020-04-26 13:14:06 -0400

    marathon-swift: skipCheck due to deprecation and open compilation issue (#557)

---
+++ Livecheckables/marathon-swift.rb
@@ -0,0 +1,6 @@
+class MarathonSwift
+  # repo deprecated in favor of official swift package manager
+  # see https://github.com/JohnSundell/Marathon/issues/208
+  # also there is un-fixed compilation issue, https://github.com/JohnSundell/Marathon/issues/151
+  livecheck :skip => "Not maintained"
+end
