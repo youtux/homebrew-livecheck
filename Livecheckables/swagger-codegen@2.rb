bcbf9dc8cffb58c0984870acef1ce7f6445ba3e0
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-02 17:27:11 -0400

    swagger-codegen@2: update regex

--- Livecheckables/swagger-codegen@2.rb
+++ Livecheckables/swagger-codegen@2.rb
@@ -1,4 +1,4 @@
 class SwaggerCodegenAT2
   livecheck :url   => "https://github.com/swagger-api/swagger-codegen.git",
-            :regex => /^v?2(?:\.\d+)+$/
+            :regex => /^v?(2(?:\.\d+)+)$/
 end
