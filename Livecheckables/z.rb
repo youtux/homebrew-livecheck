70097efe26c73f2e7c40a9f4372bae40731461f0
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-02 15:59:11 -0400

    z: update url and regex

--- Livecheckables/z.rb
+++ Livecheckables/z.rb
@@ -1,4 +1,4 @@
 class Z
-  livecheck :url   => "https://github.com/rupa/z/releases",
-            :regex => %r{Latest.*?href="/rupa/z/tree/v?([0-9\.]+)}m
+  livecheck :url   => "https://github.com/rupa/z/releases/latest",
+            :regex => %r{href=.+/tag/v?(\d+(?:\.\d+)+)}
 end
