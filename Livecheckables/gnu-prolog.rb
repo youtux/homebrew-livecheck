aa75bf5474c2dcbbf8a2f1b62226f85d2dcfb945
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-02 18:59:15 -0400

    gnu-prolog: update url and regex

--- Livecheckables/gnu-prolog.rb
+++ Livecheckables/gnu-prolog.rb
@@ -1,4 +1,4 @@
 class GnuProlog
-  livecheck :url   => "http://gprolog.univ-paris1.fr",
-            :regex => /Current stable version is gprolog-([0-9\.]+)</
+  livecheck :url   => "http://www.gprolog.org/",
+            :regex => /href=.+gprolog-v?(\d+(?:\.\d+)+)\.t/i
 end
