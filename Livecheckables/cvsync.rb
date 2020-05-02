fb2fc487aa576de48eda8dbbe30a44f8764ad5e4
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-02 19:23:03 -0400

    cvsync: update regex

--- Livecheckables/cvsync.rb
+++ Livecheckables/cvsync.rb
@@ -1,4 +1,4 @@
 class Cvsync
   livecheck :url   => "https://www.cvsync.org",
-            :regex => %r{href="http://www.cvsync.org/dist/cvsync-([0-9\.]+)\.t}
+            :regex => /href=.+cvsync-v?(\d+(?:\.\d+)+)\.t/
 end
