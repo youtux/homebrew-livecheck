fdaa0326a66a2560349ef4e51137be1031ebfe07
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-02 18:37:30 -0400

    gl2ps: update url and regex

--- Livecheckables/gl2ps.rb
+++ Livecheckables/gl2ps.rb
@@ -1,4 +1,4 @@
 class Gl2ps
-  livecheck :url   => "http://www.geuz.org/gl2ps/",
-            :regex => %r{latest stable version.*?/gl2ps-([0-9\.]+)\.t}m
+  livecheck :url   => "http://geuz.org/gl2ps/src/",
+            :regex => /href=.+gl2ps-v?(\d+(?:\.\d+)+)\.t/i
 end
