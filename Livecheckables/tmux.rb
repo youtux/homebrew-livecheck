053ee66a4dd6132d92dce23ce094a4f866dc3b30
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-02 17:29:29 -0400

    tmux: update url and regex

--- Livecheckables/tmux.rb
+++ Livecheckables/tmux.rb
@@ -1,4 +1,4 @@
 class Tmux
-  livecheck :url   => "https://github.com/tmux/tmux/releases",
-            :regex => %r{Latest.*?href="/tmux/tmux/tree/v?([0-9\.]+[0-9a-z])}m
+  livecheck :url   => "https://github.com/tmux/tmux/releases/latest",
+            :regex => %r{href=.+/tag/v?(\d+(?:\.\d+)+[a-z]?)}i
 end
