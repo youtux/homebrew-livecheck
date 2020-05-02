c4b8412b6b8a0520279444f28ff22fb0816b6026
miccal <miccal.matthews@gmail.com>
2020-05-03 04:02:31 +0800

    Update Livecheckables/aspell.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/aspell.rb
+++ Livecheckables/aspell.rb
@@ -1,4 +1,4 @@
 class Aspell
   livecheck :url   => "https://ftp.gnu.org/gnu/aspell/?C=M&O=D",
-            :regex => /href="aspell-([0-9\.]+)\.t/
+            :regex => /href=.+aspell-v?(\d+(?:\.\d+)+)\.t/
 end
