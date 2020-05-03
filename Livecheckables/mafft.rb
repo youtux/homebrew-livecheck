a69667bacfb73c68259174fbd6a84c872872a847
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-03 13:44:23 -0400

    mafft: update regex
    Closes #558.

--- Livecheckables/mafft.rb
+++ Livecheckables/mafft.rb
@@ -1,4 +1,6 @@
 class Mafft
+  # The regex below is intended to avoid releases with trailing "Experimental"
+  # text after the link for the archive.
   livecheck :url   => "https://mafft.cbrc.jp/alignment/software/source.html",
-            :regex => /mafft-([0-9\.]+)-/
+            :regex => %r{href=.+?mafft-v?(\d+(?:\.\d+)+)-with-extensions-src\.t.+?\</a\>\s*?\<(?:br[^>]*?|/li|/ul)\>}i
 end
