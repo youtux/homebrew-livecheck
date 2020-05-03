7a3a914e557a0f4c68ca7ccfb2e3f6d818b1358c
miccal <miccal.matthews@gmail.com>
2020-05-04 03:42:53 +0800

    Update Livecheckables/pass.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/pass.rb
+++ Livecheckables/pass.rb
@@ -1,4 +1,4 @@
 class Pass
-  livecheck :url   => "https://git.zx2c4.com/password-store/refs/",
-            :regex => /href=.+password-store-v?(\d+(?:\.\d+)+)\.t/
+  livecheck :url   => "https://git.zx2c4.com/password-store.git",
+            :regex => /^v?(\d+(?:\.\d+)+)$/
 end
