ba5566ab25a07943e604ae7566997eaecfe67b54
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-05 12:14:46 -0400

    unbound: update url and regex

--- Livecheckables/unbound.rb
+++ Livecheckables/unbound.rb
@@ -1,4 +1,4 @@
 class Unbound
-  livecheck :url   => "https://www.unbound.net/downloads/",
-            :regex => /href="unbound-([0-9,\.]+)\.tar/
+  livecheck :url   => "https://nlnetlabs.nl/downloads/unbound/",
+            :regex => /href=.+?unbound-v?(\d+(?:\.\d+)+)\.t/
 end
