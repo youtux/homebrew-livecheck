a0c6087811b6f13c79b9ff08b0d085c3eec6236b
miccal <miccal.matthews@gmail.com>
2020-05-06 03:14:52 +0800

    Update Livecheckables/transmission-cli.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/transmission-cli.rb
+++ Livecheckables/transmission-cli.rb
@@ -1,4 +1,4 @@
 class TransmissionCli
   livecheck :url   => "https://github.com/transmission/transmission-releases/",
-            :regex => /href=.+transmission-v?(\d+(?:\.\d+)+)\.t/i
+            :regex => /href=.+?transmission-v?(\d+(?:\.\d+)+)\.t/i
 end
