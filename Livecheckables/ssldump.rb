d0840328f65c5bab69188f71cda2642a167e2e79
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-09 21:01:13 -0400

    ssldump: add livecheckable

---
+++ Livecheckables/ssldump.rb
@@ -0,0 +1,5 @@
+class Ssldump
+  # This regex intentionally matches unstable versions, as only a beta version
+  # (0.9b3) is available at the time of writing.
+  livecheck :regex => %r{url=.+?/ssldump/([^/]+)/[^/]+\.t}
+end
