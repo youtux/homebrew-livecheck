c1a0b63e82c11011862b4aac88dd07617a22c9dd
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-10 19:27:35 -0400

    zabbix: add url and update regex

--- Livecheckables/zabbix.rb
+++ Livecheckables/zabbix.rb
@@ -1,3 +1,9 @@
 class Zabbix
-  livecheck :regex => /url=.*?zabbix-(\d+(?:\.\d+)+)\.t/
+  # As of writing, the Zabbix SourceForge repository is missing the latest
+  # version (4.4.8), so we have to check for the newest version on the Zabbix
+  # CDN index page instead. Unfortunately, the versions are separated into
+  # folders for a given major/minor version, so this will quietly stop being
+  # a proper check sometime in the future and need to be updated.
+  livecheck :url   => "https://cdn.zabbix.com/zabbix/sources/stable/4.4/",
+            :regex => /href=.+?zabbix-(\d+(?:\.\d+)+)\.t/
 end
