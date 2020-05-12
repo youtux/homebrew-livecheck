b7a9d7effc723d78e48de9e65f8de284bfd96a69
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-12 12:49:51 -0400

    ant@1.9: update url and regex

--- Livecheckables/ant@1.9.rb
+++ Livecheckables/ant@1.9.rb
@@ -1,3 +1,4 @@
 class AntAT19
-  livecheck :url => "https://www.apache.org/dist/ant/", :regex => /RELEASE\-NOTES\-(1\.9.*?)\.html/
+  livecheck :url   => "https://downloads.apache.org/ant/binaries/",
+            :regex => /href=.+?apache-ant-v?(1\.9(?:\.\d+)*)(?:-bin)?\.t/
 end
