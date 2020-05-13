798a60f8696d4edc41d18cb9411264b350385540
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-13 16:26:48 -0400

    libgit2: update url and regex

--- Livecheckables/libgit2.rb
+++ Livecheckables/libgit2.rb
@@ -1,4 +1,4 @@
 class Libgit2
-  livecheck :url   => "https://github.com/libgit2/libgit2/releases",
-            :regex => %r{href="/libgit2/libgit2/tree/v?(0\.2[789]\.[0-9\.]+)}
+  livecheck :url   => "https://github.com/libgit2/libgit2/releases/latest",
+            :regex => %r{href=.+?/tag/v?(\d+(?:\.\d+)+)}
 end
