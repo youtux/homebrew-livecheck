1f4681f36657d2d0c69945f17c9f4ec2b2caae8b
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-13 15:03:18 -0400

    oniguruma: update regex

--- Livecheckables/oniguruma.rb
+++ Livecheckables/oniguruma.rb
@@ -1,4 +1,4 @@
 class Oniguruma
   livecheck :url   => "https://github.com/kkos/oniguruma.git",
-            :regex => /^v?(\d+(?:\.\d+)+)$/
+            :regex => /^v?(\d+(?:\.\d+)+(?:.(?:mark|rev)\d+)?)$/i
 end
