0e6c5b89f2f79b470a28392bde3becb50fe121e9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-13 12:42:17 -0400

    docker-swarm: remove url and update regex

--- Livecheckables/docker-swarm.rb
+++ Livecheckables/docker-swarm.rb
@@ -1,4 +1,3 @@
 class DockerSwarm
-  livecheck :url   => "https://github.com/docker/swarm/releases",
-            :regex => %r{href="/docker/swarm/tree/v?([0-9\.]+)"}
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
