03bb02617d93d30e1ff8b2ee14568569897d00a5
alivahabi2112 <ali.vahabi1600@gmail.com>
2020-05-15 09:05:57 +0430

    Revert "wapm: update regex and add explanatory comment"
    This reverts commit ea1a510c483eb66935884845420919e895651fa0.

--- Livecheckables/wapm.rb
+++ Livecheckables/wapm.rb
@@ -1,5 +1,3 @@
 class Wapm
-  # There is a "3.2" tag in the Git repo that we avoid by only matching versions
-  # with 3+ parts (e.g., 0.1.2)
-  livecheck :regex => /^v?(\d+(?:\.\d+){2,})/
+  livecheck :regex => /^v?[^3]([\d\.]+)/
 end
