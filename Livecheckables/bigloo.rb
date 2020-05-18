dafa9469865ae5ceea29c721d2a707090fde0e3f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-18 12:08:38 -0400

    bigloo: update livecheckable

--- Livecheckables/bigloo.rb
+++ Livecheckables/bigloo.rb
@@ -1,4 +1,4 @@
 class Bigloo
   livecheck :url   => "https://www-sop.inria.fr/indes/fp/Bigloo/",
-            :regex => /Current version is .*>([0-9a-z\.]+)</
+            :regex => />\s*?version v?(\d+(?:\.\d+)+[a-z]?)\s*?</i
 end
