ef6bfebbb651213e757e5fbeff68bfaa9e729435
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-18 17:58:11 -0400

    jbig2dec: update url and regex

--- Livecheckables/jbig2dec.rb
+++ Livecheckables/jbig2dec.rb
@@ -1,4 +1,4 @@
 class Jbig2dec
-  livecheck :url   => "https://jbig2dec.com",
-            :regex => /href="[^"]*?jbig2dec-v?(\d+(?:\.\d+)+)\.t/
+  livecheck :url   => "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/latest",
+            :regex => %r{href=.+?/jbig2dec-v?(\d+(?:\.\d+)+)\.t}
 end
