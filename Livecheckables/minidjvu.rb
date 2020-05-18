b147f5ece5c249f0486152b06e85c69d550375c6
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-18 14:28:58 -0400

    minidjvu: update url and regex

--- Livecheckables/minidjvu.rb
+++ Livecheckables/minidjvu.rb
@@ -1,4 +1,4 @@
 class Minidjvu
-  livecheck :url   => "http://minidjvu.sourceforge.net",
-            :regex => /href=.+?minidjvu-v?(\d+(?:\.\d+)+)\.t/
+  livecheck :url   => "https://sourceforge.net/projects/minidjvu/",
+            :regex => %r{url=.+?/minidjvu-v?((?!0\.33)\d+(?:\.\d+)+)\.t}
 end
