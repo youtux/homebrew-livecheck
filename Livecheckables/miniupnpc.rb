9b673f4726d3869dc220407f47febf521f756ef8
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-18 18:28:03 -0400

    miniupnpc: update regex

--- Livecheckables/miniupnpc.rb
+++ Livecheckables/miniupnpc.rb
@@ -1,4 +1,6 @@
 class Miniupnpc
+  # We only match versions with only a major/minor since versions like 2.1 are
+  # stable and versions like 2.1.20191224 are unstable/development releases.
   livecheck :url   => "https://miniupnp.tuxfamily.org/files/",
-            :regex => /href='download\.php\?file=miniupnpc-([0-9]\.[0-9\.]+)\.t/
+            :regex => /href=.+?miniupnpc-v?(\d+\.\d+)\.t/
 end
