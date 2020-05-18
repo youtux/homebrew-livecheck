b34448fd3c668ba0bbc00b2582a8296f35b14dac
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-18 19:33:25 -0400

    inspircd: update url and regex

--- Livecheckables/inspircd.rb
+++ Livecheckables/inspircd.rb
@@ -1,4 +1,4 @@
 class Inspircd
-  livecheck :url   => "https://github.com/inspircd/inspircd/releases",
-            :regex => %r{latest.*?href="/inspircd/inspircd/tree/v?([0-9\.]+)}m
+  livecheck :url   => "https://github.com/inspircd/inspircd.git",
+            :regex => /^v?(\d+(?:\.\d+)+)$/
 end
