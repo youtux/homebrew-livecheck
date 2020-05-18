a8bab3c9a7761cb57df8bc92e388c6eccef95cb4
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-18 13:04:20 -0400

    libplctag: remove url and update regex

--- Livecheckables/libplctag.rb
+++ Livecheckables/libplctag.rb
@@ -1,4 +1,3 @@
 class Libplctag
-  livecheck :url   => "https://github.com/kyle-github/libplctag/releases",
-            :regex => %r{latest.*?href="/kyle-github/libplctag/tree/v?([0-9\.]+)}m
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
