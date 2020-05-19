b77d3a3b11d42a23b552a61cc3ce3a0ba43114a0
Dawid Dziurla <dawidd0811@gmail.com>
2020-05-19 17:08:21 +0200

    consul: remove url and update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/consul.rb
+++ Livecheckables/consul.rb
@@ -1,4 +1,3 @@
 class Consul
-  livecheck :url   => "https://www.consul.io/downloads.html",
-            :regex => %r{href="https://releases.hashicorp.com/consul/([0-9\.]+)}
+  livecheck :regex => /^v?(\d+(?:\.\d+)+)$/
 end
