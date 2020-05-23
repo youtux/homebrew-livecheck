e5de9a59bca0779dea7f0be56e183351cd355dff
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-23 13:40:05 -0400

    closure-compiler: update livecheckable

--- Livecheckables/closure-compiler.rb
+++ Livecheckables/closure-compiler.rb
@@ -1,5 +1,4 @@
 class ClosureCompiler
-  # livecheck :url => "https://github.com/google/closure-compiler/wiki/Binary-Downloads",
-  livecheck :url   => "https://bit.ly/2KeL7sF",
-            :regex => %r{href=".*?/compiler-([0-9\.]+)\.}
+  livecheck :url   => "https://repo1.maven.org/maven2/com/google/javascript/closure-compiler/",
+            :regex => /href=.*?v?(\d{8})/i
 end
