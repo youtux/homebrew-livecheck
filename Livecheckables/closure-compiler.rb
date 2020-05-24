99105db9aea1c6ff06d4518d3e621a85bb250ad0
nandahkrishna <nanda.harishankar@gmail.com>
2020-05-24 19:28:55 +0530

    closure-compiler: update url

--- Livecheckables/closure-compiler.rb
+++ Livecheckables/closure-compiler.rb
@@ -1,4 +1,4 @@
 class ClosureCompiler
-  livecheck :url   => "https://repo1.maven.org/maven2/com/google/javascript/closure-compiler/",
+  livecheck :url   => "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/",
             :regex => /href=.*?v?(\d{8})/i
 end
