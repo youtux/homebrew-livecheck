94bcf1a961433e07866da0bafa92ebf709191c5f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-28 12:00:01 -0400

    mu: update url and regex

--- Livecheckables/mu.rb
+++ Livecheckables/mu.rb
@@ -1,6 +1,9 @@
 class Mu
+  # We restrict matching to versions with an even-numbered minor version number,
+  # as an odd-numbered minor version number indicates a development version:
+  # https://github.com/djcb/mu/commit/23f4a64bdcdee3f9956a39b9a5a4fd0c5c2370ba
   livecheck do
-    url "https://github.com/djcb/mu/releases"
-    regex(%r{latest.*?href="/djcb/mu/tree/([0-9\.]+)}m)
+    url "https://github.com/djcb/mu.git"
+    regex(/^v?(\d+\.\d*[02468](?:\.\d+)*)$/i)
   end
 end
