f53e9dae16dd2cebc5f9c1020b4f6b704b8fc750
miccal <miccal.matthews@gmail.com>
2020-06-03 11:48:59 +0800

    Update Livecheckables/xmlto.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/xmlto.rb
+++ Livecheckables/xmlto.rb
@@ -1,6 +1,6 @@
 class Xmlto
   livecheck do
     url "https://releases.pagure.org/xmlto/?C=M&O=D"
-    regex(/href=.+xmlto-v?(\d+(?:\.\d+)+)\.t/)
+    regex(/href=.*?xmlto-v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
