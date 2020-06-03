dee9dbbafe81dfea3357ccf9b5bb2837bfefcfeb
nandahkrishna <nanda.harishankar@gmail.com>
2020-06-04 03:25:19 +0530

    popt: add livecheckable

---
+++ Livecheckables/popt.rb
@@ -0,0 +1,11 @@
+class Popt
+  # Until rpm.org releases a new version beyond 1.16, it's unclear whether
+  # checking the rpm.org archive index (http://ftp.rpm.org/mirror/popt/) would
+  # give us the latest version. As of writing this, there's a 1.18 release
+  # candidate at: http://ftp.rpm.org/releases/testing/popt-1.18-rc1.tar.gz
+  # Checking the Git repo seems like a safe bet until we see how this plays out.
+  livecheck do
+    url "https://github.com/rpm-software-management/popt.git"
+    regex(/^(?:popt)?-v?(\d+(?:[._-]\d+)+)(?:-release)?$/i)
+  end
+end
