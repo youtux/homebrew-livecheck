b880294127ea0a9b35b1007797dd2df906fa8228
miccal <miccal.matthews@gmail.com>
2020-06-07 01:27:53 +0800

    bash: update regex

--- Livecheckables/bash.rb
+++ Livecheckables/bash.rb
@@ -1,6 +1,6 @@
 class Bash
   livecheck do
     url "http://ravenports.ironwolf.systems/catalog/bucket_C8/bash/standard/"
-    regex(%r{<td id="pkgversion">([0-9.]+)</td>})
+    regex(%r{<td id="pkgversion">v?(\d+(?:\.\d+)+)(?:_\d+)?</td>})
   end
 end
