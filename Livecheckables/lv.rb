41f30a8e9b90cd6531d556829befdcb5292e92ab
nandahkrishna <nanda.harishankar@gmail.com>
2020-06-12 07:32:59 +0530

    lv: add livecheckable to skip

---
+++ Livecheckables/lv.rb
@@ -0,0 +1,8 @@
+class Lv
+  # The formula uses an archived version of the original project homepage
+  # as the original website is down. The formula is not actively maintained
+  # or developed either, so we skip checking for newer versions.
+  livecheck do
+    skip "Unable to check for versions"
+  end
+end
