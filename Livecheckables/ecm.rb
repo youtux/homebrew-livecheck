6e3dc9e947ca73cae60bb40c790b10725a45f8bf
nandahkrishna <nanda.harishankar@gmail.com>
2020-06-12 07:30:24 +0530

    ecm: add livecheckable to skip

---
+++ Livecheckables/ecm.rb
@@ -0,0 +1,8 @@
+class Ecm
+  # The first-party website for the formula has been down
+  # since 2014, and it is not possible to reliably check
+  # for newer versions.
+  livecheck do
+    skip "Unable to check for versions"
+  end
+end
