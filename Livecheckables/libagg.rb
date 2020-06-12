953384b70f62fd9103bb0373437a12eb1edd912c
nandahkrishna <nanda.harishankar@gmail.com>
2020-06-12 07:32:07 +0530

    libagg: add livecheckable to skip

---
+++ Livecheckables/libagg.rb
@@ -0,0 +1,9 @@
+class Libagg
+  # The homepage for this formula is reachable, but it does not
+  # provide any version information, while other pages are not
+  # accessible. Furthermore, the formula is not being developed
+  # or maintained, so we skip checking for new versions.
+  livecheck do
+    skip "Not actively developed or maintained"
+  end
+end
