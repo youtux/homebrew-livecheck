2b00d054bcb864ae444f12c33ac96cf79c19c342
nandahkrishna <nanda.harishankar@gmail.com>
2020-06-12 07:34:54 +0530

    pip-completion: add livecheckable

---
+++ Livecheckables/pip-completion.rb
@@ -0,0 +1,9 @@
+class PipCompletion
+  # The Formula's GitHub repo at https://github.com/ekalinin/pip-bash-completion
+  # does not have any releases or tags, so it isn't possible to check for newer
+  # versions. The Formula definition uses a 'YYYYMMDD' timestamp to indicate the
+  # version.
+  livecheck do
+    skip "No version information available"
+  end
+end
