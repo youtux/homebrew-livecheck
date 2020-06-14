cb9acc3e96aa0a2d9ff15af792da394cb607a9ca
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-14 10:31:09 -0400

    media-info: update url and regex

--- Livecheckables/media-info.rb
+++ Livecheckables/media-info.rb
@@ -1,6 +1,6 @@
 class MediaInfo
   livecheck do
-    url "https://old.mediaarea.net/download/binary/mediainfo/"
-    regex(/href=.*?v?(\d+(?:\.\d+)+)/i)
+    url "https://mediaarea.net/download/binary/mediainfo/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?[ '">]}i)
   end
 end
