0f7d699caa72bffb8d6bf98391d5d6cdaf0e8d12
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-16 23:56:56 -0400

    mecab-ipadic: add livecheckable

---
+++ Livecheckables/mecab-ipadic.rb
@@ -0,0 +1,9 @@
+class MecabIpadic
+  # We check the Debian index page because the first-party website uses a Google
+  # Drive download URL and doesn't list the version in any other way, so we
+  # can't identify the newest version there.
+  livecheck do
+    url "https://deb.debian.org/debian/pool/main/m/mecab-ipadic/"
+    regex(/href=.*?mecab-ipadic.v?(\d+(?:\.\d+)+(?:-\d+)?)(?:\+main)?\.orig\.t/i)
+  end
+end
