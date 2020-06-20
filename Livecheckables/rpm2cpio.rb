2b9dd2eff91c41e88e546df0e12066acb321fc6d
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-20 18:36:50 -0400

    rpm2cpio: update url and regex

--- Livecheckables/rpm2cpio.rb
+++ Livecheckables/rpm2cpio.rb
@@ -1,6 +1,6 @@
 class Rpm2cpio
   livecheck do
-    url "https://svnweb.freebsd.org/ports/head/archivers/rpm2cpio/Makefile?view=markup"
-    regex(%r{PORTVERSION=\s*v?(\d+(?:\.\d+)+)</td>}i)
+    url "https://svnweb.freebsd.org/ports/head/archivers/rpm2cpio/Makefile?view=co"
+    regex(/^PORTVERSION=\s*?v?(\d+(?:\.\d+)+)$/i)
   end
 end
