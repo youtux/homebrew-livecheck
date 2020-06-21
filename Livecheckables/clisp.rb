adc8e52efa7708852e2e40d588fd4c0593a4b57b
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-06-22 02:07:43 +0530

    clisp: update url and regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/clisp.rb
+++ Livecheckables/clisp.rb
@@ -1,6 +1,6 @@
 class Clisp
   livecheck do
-    url "https://ftp.gnu.org/gnu/clisp/release/"
-    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/}i)
+    url "https://ftp.gnu.org/gnu/clisp/release/?C=M&O=D"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?[ '">]}i)
   end
 end
