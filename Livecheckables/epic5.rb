1786da765dec82af5555940afba7dbe0d4ea5377
chenrui <rui@meetup.com>
2020-06-21 19:37:29 -0400

    epic5: update url and regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/epic5.rb
+++ Livecheckables/epic5.rb
@@ -1,6 +1,6 @@
 class Epic5
   livecheck do
-    url :head
-    regex(/^epic5-(\d+(?:\.\d+)+)$/i)
+    url "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/"
+    regex(/href=.*?epic5-v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
