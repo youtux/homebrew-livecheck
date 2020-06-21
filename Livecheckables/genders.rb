a9c938a53f48a2440bd7c6d0813fa77f07295731
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-21 17:29:33 -0400

    genders: update regex

--- Livecheckables/genders.rb
+++ Livecheckables/genders.rb
@@ -1,6 +1,6 @@
 class Genders
   livecheck do
     url "https://github.com/chaos/genders/releases/latest"
-    regex(%r{href=.*?/tag/genders-(\d+(?:-\d+)+)["']}i)
+    regex(%r{href=.*?/tag/genders-v?(\d+(?:-\d+)+)["']}i)
   end
 end
