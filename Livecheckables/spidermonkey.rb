bf2e4e51c70ce384ccabd00b1cdef7873d3f9c1a
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-21 17:14:47 -0400

    spidermonkey: update regex

--- Livecheckables/spidermonkey.rb
+++ Livecheckables/spidermonkey.rb
@@ -1,6 +1,6 @@
 class Spidermonkey
   livecheck do
     url "https://developer.mozilla.org/en-US/docs/Mozilla/Projects/SpiderMonkey/Releases"
-    regex(%r{href=.*?Releases/v?(\d+(?:\.\d+)*)}i)
+    regex(%r{href=.*?Releases/v?(\d+(?:\.\d+)*)[ '">]}i)
   end
 end
