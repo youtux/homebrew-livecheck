b9d93f8472030fb315cbcecf6661c5a0adc75ecb
chenrui <rui@meetup.com>
2020-06-21 20:40:20 -0400

    chocolate-doom: update url and regex
    - use official downloads page (where the artifacts reside)
    - better syntax mapping the `1.2.3/` or `1.2.3` (as there is no artifact file but rather a folder)
    -
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/chocolate-doom.rb
+++ Livecheckables/chocolate-doom.rb
@@ -1,6 +1,6 @@
 class ChocolateDoom
   livecheck do
-    url :head
-    regex(/^chocolate-doom-(\d+(?:\.\d+)+)$/i)
+    url "https://www.chocolate-doom.org/downloads/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?[ '">]}i)
   end
 end
