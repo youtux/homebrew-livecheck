f55028e2e46c69abfd8c76540bd4851a9800b63f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-22 09:31:45 -0400

    tnftp: update url and regex

--- Livecheckables/tnftp.rb
+++ Livecheckables/tnftp.rb
@@ -1,9 +1,6 @@
 class Tnftp
-  # Apple removed `tnftp` from macOS, after High Sierra (10.13).
-  # The `tnftp` Formula is built using Apple's official tarball
-  # for macOS 10.13. It's safe to assume there won't be any further
-  # updates and so we skip livecheck for this Formula.
   livecheck do
-    skip "No longer included with macOS (since 10.13)"
+    url "https://ftp.netbsd.org/pub/NetBSD/misc/tnftp/"
+    regex(/href=.*?tnftp-v?(\d+)\.t/i)
   end
 end
