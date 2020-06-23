b9d22e3141c2e65d63fea70b7997fbdacc42269e
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-06-23 07:44:36 +0530

    nexus: add livecheckable
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

---
+++ Livecheckables/nexus.rb
@@ -0,0 +1,6 @@
+class Nexus
+  livecheck do
+    url "https://help.sonatype.com/repomanager2/download/download-archives---repository-manager-oss"
+    regex(/href=.*?nexus-v?(\d+(?:\.\d+)+(?:-\d+)?)(?:-bundle)?\.t/i)
+  end
+end
