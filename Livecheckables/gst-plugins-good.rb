985052bc55f7c90a11b4d2f41ece3983133fcb3e
nandahkrishna <nanda.harishankar@gmail.com>
2020-06-24 13:11:30 +0530

    gst-plugins-good: update regex

--- Livecheckables/gst-plugins-good.rb
+++ Livecheckables/gst-plugins-good.rb
@@ -1,6 +1,6 @@
 class GstPluginsGood
   livecheck do
     url "https://gstreamer.freedesktop.org/src/gst-plugins-good/"
-    regex(/href="gst-plugins-good-([\d.]+\.[\d.]+\.[\d.]+)\.t/)
+    regex(/href="gst-plugins-good-v?(\d+\.\d*[02468](?:\.\d+)*)\.t/i)
   end
 end
