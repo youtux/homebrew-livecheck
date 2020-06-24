97cb75ba3f52f16fe10f3b7326dd20dde478cfbb
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-24 13:42:29 -0400

    tcptrace: add comment

--- Livecheckables/tcptrace.rb
+++ Livecheckables/tcptrace.rb
@@ -1,4 +1,7 @@
 class Tcptrace
+  # This site has a history of going down for periods of time, which is why the
+  # formula uses mirrors. This is something to be aware of if livecheck is
+  # unable to find versions.
   livecheck do
     url "http://www.tcptrace.org/download.shtml"
     regex(/href=.*?tcptrace-v?(\d+(?:\.\d+)+)\.t/i)
