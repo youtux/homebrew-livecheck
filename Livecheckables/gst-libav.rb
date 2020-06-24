fd121bba9f1b38698d464a7749ce18e424aa3b57
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-06-24 18:49:41 +0530

    gst-libav: update regex

--- Livecheckables/gst-libav.rb
+++ Livecheckables/gst-libav.rb
@@ -1,6 +1,6 @@
 class GstLibav
   livecheck do
     url "https://gstreamer.freedesktop.org/src/gst-libav/"
-    regex(/href="gst-libav-([\d.]+\.[\d.]+\.[\d.]+)\.t/)
+    regex(/href="gst-libav-v?(\d+\.\d*[02468](?:\.\d+)*)\.t/i)
   end
 end
