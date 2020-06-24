551d46b701ba36cccc1c668ddae9e2f031868409
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-06-24 23:19:05 +0530

    tcptrace: add livecheckable
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

---
+++ Livecheckables/tcptrace.rb
@@ -0,0 +1,9 @@
+class Tcptrace
+  # This site has a history of going down for periods of time, which is why the
+  # formula uses mirrors. This is something to be aware of if livecheck is
+  # unable to find versions.
+  livecheck do
+    url "http://www.tcptrace.org/download.shtml"
+    regex(/href=.*?tcptrace-v?(\d+(?:\.\d+)+)\.t/i)
+  end
+end
