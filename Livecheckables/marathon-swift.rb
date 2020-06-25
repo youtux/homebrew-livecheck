2e771985f89bc918f01042c64a02f9dd006ff701
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-25 12:14:58 -0400

    Delete livecheckables for removed formulae

--- Livecheckables/marathon-swift.rb
+++
@@ -1,8 +0,0 @@
-class MarathonSwift
-  # This software was deprecated in favor of the official Swift package manager
-  # and the upstream repository has been archived as of 2019-09-09
-  # (see https://github.com/JohnSundell/Marathon/issues/208).
-  livecheck do
-    skip "No longer developed/maintained"
-  end
-end
