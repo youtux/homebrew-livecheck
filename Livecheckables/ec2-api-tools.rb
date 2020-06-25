8865988137e02b3f9bfd45618cea58aec9350119
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-25 17:24:12 -0400

    ec2-api-tools: skip

--- Livecheckables/ec2-api-tools.rb
+++ Livecheckables/ec2-api-tools.rb
@@ -1,6 +1,5 @@
 class Ec2ApiTools
   livecheck do
-    url "http://s3.amazonaws.com/ec2-downloads/"
-    regex(/<Key>ec2-api-tools-v?(\d+(?:\.\d+)+(?:-\d+)?)\.zip</i)
+    skip "No longer developed/maintained"
   end
 end
