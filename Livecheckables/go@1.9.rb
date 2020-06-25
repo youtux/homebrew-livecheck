f488282cc91c78f7425aa2a8f78bc9a250db7f4b
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-25 11:58:25 -0400

    Remove deprecated livecheckables

--- Livecheckables/go@1.9.rb
+++
@@ -1,6 +0,0 @@
-class GoAT19
-  livecheck do
-    url "https://golang.org/dl/"
-    regex(/go(1\.9\.[0-9.]+)\.src/)
-  end
-end
--- Livecheckables/redis@3.2.rb
+++
@@ -1,6 +0,0 @@
-class RedisAT32
-  livecheck do
-    url "http://download.redis.io/releases/"
-    regex(/href="redis-(3\.2\.[0-9,.]+)\.t/)
-  end
-end
