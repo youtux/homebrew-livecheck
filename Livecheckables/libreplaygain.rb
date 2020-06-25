ea4fe3726296749e41417d45cfdcf1f83e6663b8
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-25 15:41:18 -0400

    libreplaygain: update regex

--- Livecheckables/libreplaygain.rb
+++ Livecheckables/libreplaygain.rb
@@ -1,6 +1,6 @@
 class Libreplaygain
   livecheck do
     url "https://www.musepack.net/index.php?pg=src"
-    regex(/href=.*?libreplaygain[._-]v?(r\d+)\.t/i)
+    regex(/href=.*?libreplaygain[._-](r\d+)\.t/i)
   end
 end
