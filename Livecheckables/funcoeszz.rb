bbc114a77095f3350b7dadc4e60babf8caaf6ad6
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-25 22:48:20 -0400

    funcoeszz: update regex

--- Livecheckables/funcoeszz.rb
+++ Livecheckables/funcoeszz.rb
@@ -1,6 +1,6 @@
 class Funcoeszz
   livecheck do
     url "https://funcoeszz.net/download/"
-    regex(/href=.*?funcoeszz.v?(\d+(?:\.\d+)+)\.s/i)
+    regex(/href=.*?funcoeszz.v?(\d+(?:\.\d+)+)\.sh/i)
   end
 end
