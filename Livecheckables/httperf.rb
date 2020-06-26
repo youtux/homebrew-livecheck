fcd9c1a226c75b02659ad513eef0a199b2d0ed4d
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-06-26 06:07:12 +0530

    httperf: skip
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/httperf.rb
+++ Livecheckables/httperf.rb
@@ -1,6 +1,7 @@
 class Httperf
+  # Until the upstream GitHub repository creates a new release (something after
+  # 0.9.0), we're unable to create a check that can identify new versions.
   livecheck do
-    url "https://www.googleapis.com/download/storage/v1/b/google-code-archive/o/v2%2Fcode.google.com%2Fhttperf%2Fdownloads-page-1.json\?\&alt\=media"
-    regex(/httperf-v?(\d+(?:\.\d+)+)\.t/i)
+    skip "No version information available to check"
   end
 end
