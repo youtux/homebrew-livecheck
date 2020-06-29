fc2b1eca77c82d7fe4b0dfe245cde6e29dd422a8
miccal <miccal.matthews@gmail.com>
2020-06-29 23:08:44 +0800

    pixman: update livecheckable
    pixman: update livecheckable

--- Livecheckables/pixman.rb
+++ Livecheckables/pixman.rb
@@ -1,6 +1,6 @@
 class Pixman
   livecheck do
-    url "https://cairographics.org/releases/"
-    regex(/href="LATEST-pixman-([\d.]+\.[\d.]+\.[\d.]+)"/)
+    url "https://cairographics.org/releases/?C=M&O=D"
+    regex(/href=.*?pixman-v?(\d+\.\d*[02468](?:\.\d+)*)\.t/i)
   end
 end
