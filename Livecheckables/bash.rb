157dd2886dcb1fa944c65187ffbbbb3baa288547
miccal <miccal.matthews@gmail.com>
2020-06-29 13:03:05 +0800

    bash & readline: update url

--- Livecheckables/bash.rb
+++ Livecheckables/bash.rb
@@ -1,6 +1,6 @@
 class Bash
   livecheck do
-    url "http://ravenports.ironwolf.systems/catalog/bucket_C8/bash/standard/"
+    url "http://www.ravenports.com/catalog/bucket_C8/bash/standard/"
     regex(%r{<td id="pkgversion">v?(\d+(?:\.\d+)+)(?:_\d+)?</td>})
   end
 end
--- Livecheckables/readline.rb
+++ Livecheckables/readline.rb
@@ -1,6 +1,6 @@
 class Readline
   livecheck do
-    url "http://ravenports.ironwolf.systems/catalog/bucket_2D/readline/standard/"
+    url "http://www.ravenports.com/catalog/bucket_2D/readline/standard/"
     regex(%r{<td id="pkgversion">v?(\d+(?:\.\d+)+)(?:_\d+)?</td>})
   end
 end
