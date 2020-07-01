6f5d5e4535c2fa513a8fb247cd1ba8d0b3731c65
Miccal Matthews <miccal.matthews@gmail.com>
2020-07-01 13:44:28 +0800

    cairomm: update url and regex

--- Livecheckables/cairomm.rb
+++ Livecheckables/cairomm.rb
@@ -1,6 +1,6 @@
 class Cairomm
   livecheck do
-    url "https://cairographics.org/releases/"
-    regex(/href=.*?cairomm-v?(\d+(?:\.\d+)+)\.t/i)
+    url "https://cairographics.org/releases/?C=M&O=D"
+    regex(/href=.*?cairomm-v?(\d+\.\d*[02468](?:\.\d+)*)\.t/i)
   end
 end
