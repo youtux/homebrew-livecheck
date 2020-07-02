3a66652cb9011850606e35388857ad707c950d3e
Miccal Matthews <miccal.matthews@gmail.com>
2020-07-03 07:39:26 +0800

    Update Livecheckables/mit-scheme.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/mit-scheme.rb
+++ Livecheckables/mit-scheme.rb
@@ -1,6 +1,6 @@
 class MitScheme
   livecheck do
     url "https://ftp.gnu.org/gnu/mit-scheme/stable.pkg/?C=M&O=D"
-    regex(/href=.*?(\d+(?:\.\d+)+)/)
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?[ '">]}i)
   end
 end
