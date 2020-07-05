5a4cfcc31f895c44052c0992b1cd8e02d9d7846c
nandahkrishna <nanda.harishankar@gmail.com>
2020-07-05 15:43:30 +0530

    libsamplerate: update regex

--- Livecheckables/libsamplerate.rb
+++ Livecheckables/libsamplerate.rb
@@ -1,6 +1,6 @@
 class Libsamplerate
   livecheck do
     url "http://www.mega-nerd.com/SRC/download.html"
-    regex(/libsamplerate-([\d.]+)\.tar\.gz"/)
+    regex(/href=.*?libsamplerate[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
