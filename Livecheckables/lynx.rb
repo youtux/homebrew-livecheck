dc1f6d71349c05ea3f6655b8ebc7a1c29ceb93a3
nandahkrishna <nanda.harishankar@gmail.com>
2020-07-05 15:55:38 +0530

    lynx: update regex

--- Livecheckables/lynx.rb
+++ Livecheckables/lynx.rb
@@ -1,6 +1,6 @@
 class Lynx
   livecheck do
     url "https://invisible-mirror.net/archives/lynx/tarballs/?C=M&O=D"
-    regex(/href="lynx([\d.]+rel\.\d+)\.tar\.gz"/)
+    regex(/href=.*?lynx[._-]?v?(\d+(?:\.\d+)+(?:rel\.\d+))\.t/i)
   end
 end
