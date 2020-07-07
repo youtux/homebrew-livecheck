d0638a8bd7c4c7660e0e3e85401cf1d0ab304cfc
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-07 10:02:49 +0530

    Change \.z to \.zip in regex

--- Livecheckables/spring-roo.rb
+++ Livecheckables/spring-roo.rb
@@ -1,6 +1,6 @@
 class SpringRoo
   livecheck do
     url :homepage
-    regex(%r{href=".*?/spring-roo-([0-9.]+)\.RELEASE\.z})
+    regex(%r{href=".*?/spring-roo-([0-9.]+)\.RELEASE\.zip})
   end
 end
