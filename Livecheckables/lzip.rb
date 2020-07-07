15cfdbc9d7b35ce65955b3ded472c3df688d0da4
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-07 09:41:43 +0530

    lzip: update regex

--- Livecheckables/lzip.rb
+++ Livecheckables/lzip.rb
@@ -1,6 +1,6 @@
 class Lzip
   livecheck do
     url "https://download.savannah.gnu.org/releases/lzip/"
-    regex(/href="lzip-([0-9,.]+).tar.lz"/)
+    regex(/href=.*?lzip[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
