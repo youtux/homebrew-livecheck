8de93bfe6b6c53e3416018caacc97c72c3ff3e59
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-06 23:08:52 -0400

    iozone: update regex

--- Livecheckables/iozone.rb
+++ Livecheckables/iozone.rb
@@ -1,6 +1,6 @@
 class Iozone
   livecheck do
     url "http://www.iozone.org/src/current"
-    regex(/href=.*?iozone[._-]?v?(\d+(?:[._-]\d+)+)\.t/i)
+    regex(/href=.*?iozone[._-]?v?(\d+(?:[._]\d+)+)\.t/i)
   end
 end
