179912aa91d648255e6e4a8cbe0a5d41d3ddcb97
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-07 13:07:27 +0530

    libsamplerate: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/libsamplerate.rb
+++ Livecheckables/libsamplerate.rb
@@ -1,6 +1,6 @@
 class Libsamplerate
   livecheck do
     url "http://www.mega-nerd.com/SRC/download.html"
-    regex(/href=.*?libsamplerate[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    regex(/href=.*?libsamplerate[._-]v?(\d+(?:\.\d+)+)\.t[^.]+(?:\.[^.]+)?["' >]/i)
   end
 end
