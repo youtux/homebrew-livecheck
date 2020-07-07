1490a69d50ce8357c1026d07b59cf97a66bc3762
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-07 09:44:01 +0530

    siege: update regex

--- Livecheckables/siege.rb
+++ Livecheckables/siege.rb
@@ -1,6 +1,6 @@
 class Siege
   livecheck do
     url "http://download.joedog.org/siege/?C=M&O=D"
-    regex(/href="siege-([\d.]+)\.tar\.gz"/)
+    regex(/href=.*?siege[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
