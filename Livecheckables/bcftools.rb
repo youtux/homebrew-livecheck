cf10c41c20343d3a8f8b2c7d6ff35530ff936a47
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-07 12:55:26 +0530

    bcftools: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/bcftools.rb
+++ Livecheckables/bcftools.rb
@@ -1,6 +1,6 @@
 class Bcftools
   livecheck do
     url "https://github.com/samtools/bcftools/releases/latest"
-    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["']}i)
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 end
