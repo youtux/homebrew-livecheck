12a7e36a5746b58f50224f29534879fe450b7bb1
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-07 09:43:00 +0530

    rsync: update regex

--- Livecheckables/rsync.rb
+++ Livecheckables/rsync.rb
@@ -1,6 +1,6 @@
 class Rsync
   livecheck do
     url "https://rsync.samba.org/ftp/rsync/?C=M&O=D"
-    regex(/href="rsync-([\d.]+)\.tar\.gz"/)
+    regex(/href=.*?rsync[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
