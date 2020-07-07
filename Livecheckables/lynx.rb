e605f4c7e4621c6c4931a8192799d8353d7e5d26
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-07 00:05:35 -0400

    lynx: update regex

--- Livecheckables/lynx.rb
+++ Livecheckables/lynx.rb
@@ -1,6 +1,6 @@
 class Lynx
   livecheck do
     url "https://invisible-mirror.net/archives/lynx/tarballs/?C=M&O=D"
-    regex(/href=.*?lynx[._-]?v?(\d+(?:\.\d+)+(?:rel\.\d+))\.t/i)
+    regex(/href=.*?lynx[._-]?v?(\d+(?:\.\d+)+(?:rel\.?\d+))\.t/i)
   end
 end
