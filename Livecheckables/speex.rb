c2849e4defe09892ad183151d7c301ee65456bcb
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-07 09:44:59 +0530

    speex: update regex

--- Livecheckables/speex.rb
+++ Livecheckables/speex.rb
@@ -1,6 +1,6 @@
 class Speex
   livecheck do
     url "https://ftp.osuosl.org/pub/xiph/releases/speex/?C=M&O=D"
-    regex(/href="speex-([\d.]+)\.tar\.gz"/)
+    regex(/href=.*?speex[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
