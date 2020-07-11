f1e6698b5eef1c8eab2d3e387b9731ef1c3ef63f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-11 10:03:43 -0400

    Integrate strategy functions into strategies

--- livecheck/livecheck_strategy/page_match.rb
+++ livecheck/livecheck_strategy/page_match.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
+require "open-uri"
+
 module LivecheckStrategy
   class PageMatch
     NAME = name.demodulize
     NICE_NAME = "Page match"
     PRIORITY = 0
+
+    def self.page_matches(url, regex)
+      page = URI.open(url).read
+      matches = page.scan(regex)
+      matches.map(&:first).uniq
+    end
+    private_class_method :page_matches
 
     def self.find_versions(url, regex)
       match_data = { :matches => {}, :regex => regex, :url => url }
