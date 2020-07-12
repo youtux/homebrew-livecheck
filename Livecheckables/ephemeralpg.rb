6662caeb38cb2672252237ed417a3889582e8e59
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-12 18:20:29 -0400

    ephemeralpg: update url and regex

--- Livecheckables/ephemeralpg.rb
+++ Livecheckables/ephemeralpg.rb
@@ -1,6 +1,6 @@
 class Ephemeralpg
   livecheck do
-    url :homepage
-    regex(/href=['"][^'"]*?ephemeralpg-(\d+(?:\.\d+)+)\.t/)
+    url "https://eradman.com/ephemeralpg/code/"
+    regex(/href=.*?ephemeralpg[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
