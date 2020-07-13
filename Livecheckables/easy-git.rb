a23dcc729c53255040b9ee9bc44526fd7ebe1b27
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-12 23:51:05 -0400

    easy-git: update regex

--- Livecheckables/easy-git.rb
+++ Livecheckables/easy-git.rb
@@ -1,6 +1,6 @@
 class EasyGit
   livecheck do
     url "https://people.gnome.org/~newren/eg/download/"
-    regex(%r{href="(\d+(?:\.\d+)+)/eg"})
+    regex(%r{href=.*?(\d+(?:\.\d+)+)/eg["' >]}i)
   end
 end
