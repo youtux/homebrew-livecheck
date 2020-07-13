21f36d07d6bacf68502d28644a6bccbb6217f35a
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-13 22:14:25 +0530

    Change href=".* and href=".*? to href=.*?

--- Livecheckables/cfr-decompiler.rb
+++ Livecheckables/cfr-decompiler.rb
@@ -1,6 +1,6 @@
 class CfrDecompiler
   livecheck do
     url :homepage
-    regex(/href=".*cfr-([0-9_.]+)\.jar/)
+    regex(/href=.*?cfr-([0-9_.]+)\.jar/)
   end
 end
--- Livecheckables/guile.rb
+++ Livecheckables/guile.rb
@@ -1,6 +1,6 @@
 class Guile
   livecheck do
     url "https://ftp.gnu.org/gnu/guile/"
-    regex(/href=".*guile-([\d.]+\.[\d.]+\.[\d.]+)\.t/)
+    regex(/href=.*?guile-([\d.]+\.[\d.]+\.[\d.]+)\.t/)
   end
 end
