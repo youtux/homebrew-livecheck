60cef88b012aa0b2af87fca0d273fe268b9bd69d
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-12 23:57:37 -0400

    libnatpmp: update regex

--- Livecheckables/libnatpmp.rb
+++ Livecheckables/libnatpmp.rb
@@ -1,6 +1,6 @@
 class Libnatpmp
   livecheck do
     url "http://miniupnp.free.fr/files/"
-    regex(/href='download.php\?file=libnatpmp-([0-9]+)\.t/)
+    regex(/href=.*?libnatpmp[._-]v?(\d{6,8})\.t/i)
   end
 end
