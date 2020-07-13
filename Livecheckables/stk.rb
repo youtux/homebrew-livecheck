6755a8104b514a2b20c145a68654e9817399b44e
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-13 15:08:28 -0400

    stk: fix regex literal syntax

--- Livecheckables/stk.rb
+++ Livecheckables/stk.rb
@@ -1,6 +1,6 @@
 class Stk
   livecheck do
     url "https://ccrma.stanford.edu/software/stk/download.html"
-    regex(%r{href=.*?stk[._-]v?(\d+(?:\.\d+)+)\.t}i)
+    regex(/href=.*?stk[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
