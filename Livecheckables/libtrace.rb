355c9249c233bd27369a35faeaf5c82090bf084c
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-13 02:00:09 -0400

    libtrace: update regex

--- Livecheckables/libtrace.rb
+++ Livecheckables/libtrace.rb
@@ -1,6 +1,6 @@
 class Libtrace
   livecheck do
     url :homepage
-    regex(%r{stable version is.*?href=".*?/libtrace-([0-9.]+)\.t})
+    regex(/href=.*?libtrace[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
