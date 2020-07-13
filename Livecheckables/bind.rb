78405b7ff44125bd6bc5f9ac778eb7679c9b232d
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-12 20:19:10 -0400

    bind: update regex

--- Livecheckables/bind.rb
+++ Livecheckables/bind.rb
@@ -1,6 +1,8 @@
 class Bind
+  # BIND indicates stable releases with an even-numbered minor (e.g., x.2.x)
+  # and the regex below only matches these versions.
   livecheck do
     url "https://www.isc.org/downloads/"
-    regex(/Current-Stable.*?href=".*?bind-([A-Za-z0-9.\-]+)\.t/m)
+    regex(/href=.*?bind[._-]v?(\d+\.\d*[02468](?:\.\d+)*)\.t/i)
   end
 end
