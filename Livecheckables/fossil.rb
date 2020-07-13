f46b2be8cf888f3173de084831cc66681c59945f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-13 13:39:39 -0400

    fossil: update regex

--- Livecheckables/fossil.rb
+++ Livecheckables/fossil.rb
@@ -1,6 +1,6 @@
 class Fossil
   livecheck do
     url "https://www.fossil-scm.org/home/uv/download.js"
-    regex(/"title": *?"Version (\d+(?:\.\d+)+)"/)
+    regex(/"title": *?"Version (\d+(?:\.\d+)+) circa/i)
   end
 end
