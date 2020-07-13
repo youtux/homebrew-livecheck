877d4bff4a8c03b2c73d39e14961261ea660e70f
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-13 10:51:06 +0530

    python@3.8: update url and regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/python@3.8.rb
+++ Livecheckables/python@3.8.rb
@@ -1,6 +1,6 @@
 class PythonAT38
   livecheck do
-    url "https://www.python.org/downloads/"
-    regex(%r{href=.*?ftp/python/(\d+(?:\.\d+)+)/}i)
+    url "https://www.python.org/ftp/python/"
+    regex(%r{href=.*?v?(3\.8(?:\.\d+)*)/?["' >]}i)
   end
 end
