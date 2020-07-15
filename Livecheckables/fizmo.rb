1c63125ab335250500085af01a30c137196e52f8
nandahkrishna <nanda.harishankar@gmail.com>
2020-07-16 03:03:47 +0530

    fizmo: update regex

--- Livecheckables/fizmo.rb
+++ Livecheckables/fizmo.rb
@@ -1,6 +1,6 @@
 class Fizmo
   livecheck do
     url "https://fizmo.spellbreaker.org/download/"
-    regex(%r{href="../source/fizmo-([0-9.]+)\.t.*?current branch}m)
+    regex(%r{href=.*?/fizmo[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
 end
