a39a010b62b53774e7e3047fd7c623139412d259
nandahkrishna <nanda.harishankar@gmail.com>
2020-07-16 01:54:32 +0530

    bcpp: update regex

--- Livecheckables/bcpp.rb
+++ Livecheckables/bcpp.rb
@@ -1,6 +1,6 @@
 class Bcpp
   livecheck do
     url "https://invisible-island.net/bcpp/CHANGES.html"
-    regex(/href="#index-t(2[0-9]+)"/)
+    regex(/href=.*?index[._-]t(\d{6,8})["' >]/i)
   end
 end
