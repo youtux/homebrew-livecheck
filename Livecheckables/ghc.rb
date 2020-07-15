887ab558081c8398d2c37d624607fbc3f22ab174
nandahkrishna <nanda.harishankar@gmail.com>
2020-07-16 04:09:47 +0530

    ghc: update url and regex

--- Livecheckables/ghc.rb
+++ Livecheckables/ghc.rb
@@ -1,6 +1,6 @@
 class Ghc
   livecheck do
-    url "https://www.haskell.org/ghc/download.html"
-    regex(/href="download_ghc_(?:\d+(?:_?\d+)+).html">(\d+(?:\.\d+)+)</)
+    url "https://downloads.haskell.org/~ghc/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 end
