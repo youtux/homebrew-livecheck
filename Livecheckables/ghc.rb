0760ffc813c9c44bb0bf21bace9321beec3ac6f5
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-17 02:15:48 +0530

    ghc: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/ghc.rb
+++ Livecheckables/ghc.rb
@@ -1,6 +1,6 @@
 class Ghc
   livecheck do
     url "https://downloads.haskell.org/~ghc/"
-    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
+    regex(%r{href=.*?v?(\d+(?:\.\d{1,4})+)/?["' >]}i)
   end
 end
