5adb3862ed642a1ac8325922138b4c0edc05c8a1
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-17 02:14:15 +0530

    gcc: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/gcc.rb
+++ Livecheckables/gcc.rb
@@ -1,6 +1,6 @@
 class Gcc
   livecheck do
     url "https://ftp.gnu.org/gnu/gcc/?C=M&O=D"
-    regex(%r{href="gcc-(\d+(?:\.\d+)+)(?:/?"|\.t)})
+    regex(%r{href=.*?gcc[._-]v?(\d+(?:\.\d+)+)(?:/?["' >]|\.t)}i)
   end
 end
