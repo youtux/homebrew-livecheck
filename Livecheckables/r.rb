5dafe4263ea574ad81d7efb757009223b9dfb4cf
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-17 03:46:05 +0530

    r: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/r.rb
+++ Livecheckables/r.rb
@@ -1,6 +1,6 @@
 class R
   livecheck do
     url "https://cran.rstudio.com/banner.shtml"
-    regex(/href=.*?R[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    regex(%r{href=(?:["']?|.*?/)R[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
 end
