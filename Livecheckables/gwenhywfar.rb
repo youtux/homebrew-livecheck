af1c66b8ca4469c87368ecb95ab392eb21c38b63
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-17 02:18:58 +0530

    gwenhywfar: update regex

--- Livecheckables/gwenhywfar.rb
+++ Livecheckables/gwenhywfar.rb
@@ -1,6 +1,6 @@
 class Gwenhywfar
   livecheck do
     url "https://www.aquamaniac.de/rdm/projects/gwenhywfar/files"
-    regex(%r{href="[^"]+/gwenhywfar-(\d+(?:\.\d+)+)\.t})
+    regex(/href=.*?gwenhywfar[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
