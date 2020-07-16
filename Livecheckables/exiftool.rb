be1929f0684b99f75e870fcb19224726ddcf21c4
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-17 00:54:01 +0530

    exiftool: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/exiftool.rb
+++ Livecheckables/exiftool.rb
@@ -1,6 +1,6 @@
 class Exiftool
   livecheck do
     url "https://exiftool.org/history.html"
-    regex(/href=.*?Image[._-]ExifTool[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    regex(/production release is.*?href=.*?Image[._-]ExifTool[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
