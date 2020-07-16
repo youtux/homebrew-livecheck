afec213f9e9fcc1db5b83b7ccf1e989228c73c49
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-17 04:16:47 +0530

    terraform: update url and regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/terraform.rb
+++ Livecheckables/terraform.rb
@@ -1,6 +1,6 @@
 class Terraform
   livecheck do
-    url "https://www.terraform.io/downloads.html"
-    regex(%r{href="https://releases.hashicorp.com/terraform/([0-9.]+)})
+    url "https://releases.hashicorp.com/terraform/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 end
