6f089751f706f39f1601bea06020aec9362abec8
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-16 22:55:23 +0530

    bcpp: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/bcpp.rb
+++ Livecheckables/bcpp.rb
@@ -1,6 +1,6 @@
 class Bcpp
   livecheck do
     url "https://invisible-island.net/bcpp/CHANGES.html"
-    regex(/href=.*?index[._-]t(\d{6,8})["' >]/i)
+    regex(/id=.*?t(\d{6,8})["' >]/im)
   end
 end
