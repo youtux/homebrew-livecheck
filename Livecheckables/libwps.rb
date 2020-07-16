3b2fbd9a406e8cea8b5f8d3e7ba33661f6db1c7d
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-16 12:36:37 +0530

    libwps: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/libwps.rb
+++ Livecheckables/libwps.rb
@@ -1,6 +1,6 @@
 class Libwps
   livecheck do
     url "https://sourceforge.net/projects/libwps/files/libwps/"
-    regex(%r{href=.*?libwps[._-]v?(\d+(?:\.\d+)+)/?["' >]}i)
+    regex(%r{href=.*?libwps(?:/|[._-])v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 end
