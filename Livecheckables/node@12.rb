a7d9850a93b0ed447c85a822c30f01c8ab64f689
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-16 19:16:12 -0400

    Align HTML attribute delimiters with current usage

--- Livecheckables/node@12.rb
+++ Livecheckables/node@12.rb
@@ -1,6 +1,6 @@
 class NodeAT12
   livecheck do
     url "https://nodejs.org/dist/"
-    regex(%r{href=.*?v?(12(?:\.\d+){2})/?['"]})
+    regex(%r{href=.*?v?(12(?:\.\d+){2})/?["' >]}i)
   end
 end
--- Livecheckables/ttyrec.rb
+++ Livecheckables/ttyrec.rb
@@ -1,6 +1,6 @@
 class Ttyrec
   livecheck do
     url :homepage
-    regex(/href=['"]?ttyrec-v?(\d+(?:\.\d+)+)\.t/i)
+    regex(/href=["']?ttyrec[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
