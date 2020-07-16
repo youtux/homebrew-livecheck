97d1761884df5f697ecac2aaf66a4460c37e5e37
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-17 02:47:53 +0530

    opus: update regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/opus.rb
+++ Livecheckables/opus.rb
@@ -1,6 +1,6 @@
 class Opus
   livecheck do
     url "https://archive.mozilla.org/pub/opus/"
-    regex(/href=.*?opus[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    regex(%r{href=(?:["']?|.*?/)opus[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
 end
