1d5eef3a7ccdb98f8aa609a808586cb5353d6cf6
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-17 13:27:27 -0400

    gabedit: update url and regex

--- Livecheckables/gabedit.rb
+++ Livecheckables/gabedit.rb
@@ -1,6 +1,8 @@
 class Gabedit
+  # Consider switching back to checking SourceForge releases once we can alter
+  # the matched version from `250` to `2.5.0`.
   livecheck do
-    url "https://sourceforge.net/projects/gabedit/"
-    regex(%r{/gabedit/Gabedit([0-9]+)/})
+    url "https://sites.google.com/site/allouchear/Home/gabedit/download"
+    regex(/current stable version of gabedit is v?(\d+(?:\.\d+)+)/i)
   end
 end
