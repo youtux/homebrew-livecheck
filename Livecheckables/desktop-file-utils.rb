5a7f6b1fbe95df9ffc886d27786ba9378e314669
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-17 11:11:34 -0400

    desktop-file-utils: delete livecheckable

--- Livecheckables/desktop-file-utils.rb
+++
@@ -1,6 +0,0 @@
-class DesktopFileUtils
-  livecheck do
-    url "https://www.freedesktop.org/software/desktop-file-utils/releases/"
-    regex(/href=.*?desktop-file-utils-v?(\d+(?:\.\d+)+)\.t/i)
-  end
-end
