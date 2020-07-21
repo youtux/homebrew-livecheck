ef07c2a56f5525c7df5d0b2bcb7c7eaa6d983e46
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-20 22:23:23 -0400

    dcmtk: update regex

--- Livecheckables/dcmtk.rb
+++ Livecheckables/dcmtk.rb
@@ -1,6 +1,6 @@
 class Dcmtk
   livecheck do
     url "https://dicom.offis.de/download/dcmtk/release/"
-    regex(/href=.*?dcmtk-([0-9._]+)\.t/i)
+    regex(/href=.*?dcmtk[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
