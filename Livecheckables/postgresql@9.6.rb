a8b7f9332f8fbe320d86743c8f6b620d339a7bcf
nandahkrishna <nanda.harishankar@gmail.com>
2020-07-27 21:35:31 +0530

    Update regex for postgresql@9.5 and postgresql@9.6

--- Livecheckables/postgresql@9.6.rb
+++ Livecheckables/postgresql@9.6.rb
@@ -1,6 +1,6 @@
 class PostgresqlAT96
   livecheck do
     url "https://www.postgresql.org/docs/9.6/static/release.html"
-    regex(/Release ([0-9,.]+)/i)
+    regex(/Release v?(\d+(?:\.\d+)+)/i)
   end
 end
