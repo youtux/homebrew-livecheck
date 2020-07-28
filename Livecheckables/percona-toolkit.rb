6199ed266825ee53e368a0808910b2af416bcb94
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-28 23:40:39 +0530

    percona-toolkit: update regex

--- Livecheckables/percona-toolkit.rb
+++ Livecheckables/percona-toolkit.rb
@@ -1,6 +1,6 @@
 class PerconaToolkit
   livecheck do
     url "https://www.percona.com/downloads/percona-toolkit/LATEST/"
-    regex(%r{value="percona-toolkit/([0-9\-.]+)"}i)
+    regex(%r{value=.*?percona-toolkit/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 end
