7b4d0e055fd6c0f96896ef05b7621ba604a76046
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-28 16:52:07 -0400

    autossh: update regex

--- Livecheckables/autossh.rb
+++ Livecheckables/autossh.rb
@@ -1,6 +1,6 @@
 class Autossh
   livecheck do
     url :homepage
-    regex(/href=.*?autossh[._-]v?(\d+(?:\.\d+)+[a-z]+)\.t/i)
+    regex(/href=.*?autossh[._-]v?(\d+(?:\.\d+)+[a-z]?)\.t/i)
   end
 end
