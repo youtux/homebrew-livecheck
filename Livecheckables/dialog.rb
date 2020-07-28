bb5437ff9f19b5f8073dbb65fe752e1bc8e2b4a3
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-28 23:30:12 +0530

    dialog: update regex

--- Livecheckables/dialog.rb
+++ Livecheckables/dialog.rb
@@ -1,6 +1,6 @@
 class Dialog
   livecheck do
     url "http://www.invisible-mirror.net/archives/dialog/"
-    regex(/dialog-([0-9.-]+)\.t/i)
+    regex(/href=.*?dialog[._-]v?(\d+(?:\.\d+)+-\d{6,8})\.t/i)
   end
 end
