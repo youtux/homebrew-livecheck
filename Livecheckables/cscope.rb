69582d7568e6490efbe156a4ec4e8b89ce72f434
Nanda H Krishna <nanda.harishankar@gmail.com>
2020-07-29 21:40:13 +0530

    Update regexes containing `a-z` and `0-9`
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/cscope.rb
+++ Livecheckables/cscope.rb
@@ -1,6 +1,6 @@
 class Cscope
   livecheck do
     url "https://sourceforge.net/projects/cscope/rss"
-    regex(/cscope-v?(\d+(?:\.\d+)+[\da-z]*)\.t/i)
+    regex(/cscope-v?(\d+(?:\.\d+)+[a-z]?)\.t/i)
   end
 end
--- Livecheckables/isc-dhcp.rb
+++ Livecheckables/isc-dhcp.rb
@@ -1,6 +1,6 @@
 class IscDhcp
   livecheck do
     url "https://www.isc.org/downloads/"
-    regex(%r{/dhcp-v?(\d+(?:\.\d+)+)\.t}i)
+    regex(%r{/dhcp-v?(\d+(?:\.\d+)+(?:-P\d+)?)\.t}i)
   end
 end
