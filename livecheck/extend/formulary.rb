60cab1a7b205762ac8e62f8a7a960e66f20f375f
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-30 13:01:35 +0200

    extend/formulary: fix formula loading
    due to recent changes in brew:
    https://github.com/Homebrew/brew/commit/24eff8f81ac6bf50916a61bfa28fb9e4d950499a
    https://github.com/Homebrew/brew/commit/08e35e9cb4e65c6969016ffaa8af4e1a968ca496
    https://github.com/Homebrew/brew/commit/e07b02fde2a423ef6389330a80f4c3e5e71fcfed

--- livecheck/extend/formulary.rb
+++ livecheck/extend/formulary.rb
@@ -11,10 +11,20 @@ module Formulary
       end
     end
 
-    def load_formula(name, path, contents, namespace)
+    def load_formula(name, path, contents, namespace, flags:)
+      raise "Formula loading disabled by HOMEBREW_DISABLE_LOAD_FORMULA!" if Homebrew::EnvConfig.disable_load_formula?
+
       mod = Module.new
       const_set(namespace, mod)
-      mod.module_eval(contents, path)
+
+      begin
+        mod.const_set(:BUILD_FLAGS, flags)
+        mod.module_eval(contents, path)
+      rescue NameError, ArgumentError, ScriptError => e
+        $stderr.puts e.backtrace if Homebrew::EnvConfig.developer?
+        raise FormulaUnreadableError.new(name, e)
+      end
+      class_name = class_s(name)
 
       lc_path = livecheckable_path(path)
       if lc_path&.exist?
@@ -22,12 +32,14 @@ module Formulary
         mod.module_eval(lc_path.read, lc_path)
       end
 
-      class_name = class_s(name)
-
       begin
         mod.const_get(class_name)
       rescue NameError => e
-        raise FormulaUnavailableError, name, e.backtrace
+        class_list = mod.constants
+                        .map { |const_name| mod.const_get(const_name) }
+                        .select { |const| const.is_a?(Class) }
+        new_exception = FormulaClassUnavailableError.new(name, path, class_name, class_list)
+        raise new_exception, "", e.backtrace
       end
     end
   end
