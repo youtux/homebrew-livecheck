41764b0f2b34aa8cf8be2db64205eacfa5f04198
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-31 00:14:44 -0400

    popt: update regex and comment

--- Livecheckables/popt.rb
+++ Livecheckables/popt.rb
@@ -1,11 +1,10 @@
 class Popt
-  # Until rpm.org releases a new version beyond 1.16, it's unclear whether
-  # checking the rpm.org archive index (http://ftp.rpm.org/mirror/popt/) would
-  # give us the latest version. As of writing this, there's a 1.18 release
-  # candidate at: http://ftp.rpm.org/releases/testing/popt-1.18-rc1.tar.gz
-  # Checking the Git repo seems like a safe bet until we see how this plays out.
+  # The stable archive is found at http://ftp.rpm.org/popt/releases/popt-1.x/
+  # but it's unclear whether this would be a reliable check in the long term.
+  # We're simply checking the Git repository tags for the moment, as we
+  # shouldn't encounter problems with this method.
   livecheck do
     url :homepage
-    regex(/^(?:popt)?[._-]v?(\d+(?:[._-]\d+)+)(?:-release)?$/i)
+    regex(/^(?:popt[._-])?v?(\d+(?:[._-]\d+)+)(?:-release)?$/i)
   end
 end
