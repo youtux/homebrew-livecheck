a3f3db6642cffc0b9c87f29cec908ba195a9cd1f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-09 12:04:48 -0400

    Remove Sourceforge strategy SPECIAL_CASES

--- Livecheckables/bashdb.rb
+++ Livecheckables/bashdb.rb
@@ -1,6 +1,7 @@
 class Bashdb
   livecheck do
     url "https://sourceforge.net/projects/bashdb/files/bashdb/"
+    strategy :page_match
     regex(%r{href=(?:["']|.*?bashdb/)?v?(\d+(?:[.-]\d+)+)/?["' >]}i)
   end
 end
