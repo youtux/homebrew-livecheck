024d1ed58bc8aa4295f403d7a4f4ec902ab28fd9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-10 01:05:43 -0400

    Gnu: update regex in match?

--- Livecheckables/cvs.rb
+++ Livecheckables/cvs.rb
@@ -1,7 +1,6 @@
 class Cvs
   livecheck do
     url "https://ftp.gnu.org/non-gnu/cvs/source/feature/"
-    strategy :page_match
     regex(%r{href=.*?v?(\d+(?:\.\d+)+)/}i)
   end
 end
---
+++ Livecheckables/podiff.rb
@@ -0,0 +1,6 @@
+class Podiff
+  livecheck do
+    url "https://download.gnu.org.ua/pub/release/podiff/"
+    regex(/href=.*?podiff[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+end
