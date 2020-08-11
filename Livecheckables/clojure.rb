699809b7098b10f78786a9a00e6499c3310c37a0
chenrui <chenrui333@gmail.com>
2020-08-11 11:54:13 -0400

    clojure: update url and regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/clojure.rb
+++ Livecheckables/clojure.rb
@@ -1,6 +1,6 @@
 class Clojure
   livecheck do
-    url "https://github.com/clojure/brew-install.git"
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    url "https://raw.githubusercontent.com/clojure/homebrew-tools/master/Formula/clojure.rb"
+    regex(/url ".*?clojure-tools-v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
