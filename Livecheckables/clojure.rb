10b4660a1543568beee7bdd13d948c71549aedfc
chenrui <rui@meetup.com>
2020-08-11 11:50:46 -0400

    clojure: consistent with the tap
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/clojure.rb
+++ Livecheckables/clojure.rb
@@ -1,6 +1,6 @@
 class Clojure
   livecheck do
-    url "https://clojure.org/guides/getting_started"
-    regex(/linux-install-v?(\d+(?:\.\d+)+)/i)
+    url "https://raw.githubusercontent.com/clojure/homebrew-tools/master/Formula/clojure.rb"
+    regex(/url ".*?clojure-tools-v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
