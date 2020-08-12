29c67096ae84ac146862826ade91802deb1142a6
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-12 11:33:29 -0400

    Hackage: handle downloads.haskell.org URLs

--- livecheck/livecheck_strategy/hackage.rb
+++ livecheck/livecheck_strategy/hackage.rb
@@ -3,11 +3,11 @@
 module LivecheckStrategy
   class Hackage
     def self.match?(url)
-      /hackage\.haskell\.org/.match?(url)
+      /(?:downloads|hackage)\.haskell\.org/i.match?(url)
     end
 
     def self.find_versions(url, regex = nil)
-      package_name = ((url.split("/")[4]).split("-")[0..-2]).join("-")
+      /^(?<package_name>.+?)-\d+/i =~ File.basename(url)
 
       page_url = "https://hackage.haskell.org/package/#{package_name}/src"
       regex ||= %r{<h3>#{package_name}-(.*?)/?</h3>}i
