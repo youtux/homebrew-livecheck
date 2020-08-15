151a38cea6a3b8baa6a4774c144b8ef1b08eb248
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-15 10:15:58 -0400

    Add/update node livecheckables

---
+++ Livecheckables/node.rb
@@ -0,0 +1,6 @@
+class Node
+  livecheck do
+    url "https://nodejs.org/dist/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+end
--- Livecheckables/node@12.rb
+++ Livecheckables/node@12.rb
@@ -1,6 +1,6 @@
 class NodeAT12
   livecheck do
     url "https://nodejs.org/dist/"
-    regex(%r{href=.*?v?(12(?:\.\d+){2})/?["' >]}i)
+    regex(%r{href=["']?v?(12(?:\.\d+)+)/?["' >]}i)
   end
 end
