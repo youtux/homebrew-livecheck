0f0445bb07a57de96c0387b3b0a56071684d6ffb
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-15 10:23:38 -0400

    Add/update node livecheckables

--- Livecheckables/node@10.rb
+++ Livecheckables/node@10.rb
@@ -1,6 +1,6 @@
 class NodeAT10
   livecheck do
-    url "https://nodejs.org/en/download/releases/"
-    regex(%r{<td data-label="Version">Node.js v?(10(?:\.\d+)+)</td>}i)
+    url "https://nodejs.org/dist/"
+    regex(%r{href=["']?v?(10(?:\.\d+)+)/?["' >]}i)
   end
 end
