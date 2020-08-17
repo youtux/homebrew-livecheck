b59f650a28fbb241ca299fee85968de1427d1831
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-17 16:27:41 -0400

    Bitbucket: Update regex

--- livecheck/livecheck_strategy/bitbucket.rb
+++ livecheck/livecheck_strategy/bitbucket.rb
@@ -10,12 +10,15 @@ module LivecheckStrategy
       path, kind, suffix =
         url.match(%r{bitbucket\.org/(.+?)/(get|downloads)/(?:.*?[-_])?v?\d+(?:\.\d+)+([^/]+)})[1, 3]
 
+      # Use `\.t` instead of specific tarball extensions (e.g., .tar.gz)
+      suffix.sub!(/\.t(?:ar\..+|[a-z0-9]+)$/, "\.t")
+
       page_url = if kind == "get"
         "https://bitbucket.org/#{path}/downloads/?tab=tags"
       else
         "https://bitbucket.org/#{path}/downloads/"
       end
-      regex ||= /(\d+(?:\.\d+)+)#{Regexp.escape(suffix)}"/
+      regex ||= /href=.*?v?(\d+(?:\.\d+)+)#{Regexp.escape(suffix)}/i
 
       PageMatch.find_versions(page_url, regex)
     end
