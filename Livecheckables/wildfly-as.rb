a5e0cf34651101204f428355d37be87a03ad557a
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-18 19:12:21 -0400

    wildfly-as: update regex

--- Livecheckables/wildfly-as.rb
+++ Livecheckables/wildfly-as.rb
@@ -1,6 +1,6 @@
 class WildflyAs
   livecheck do
     url "https://wildfly.org/downloads/"
-    regex(/href=.*?wildfly[._-]v?(\d+(?:\.\d+)+\.Final)\.t/i)
+    regex(/href=.*?wildfly[._-]v?(\d+(?:\.\d+)+)\.Final\.t/i)
   end
 end
