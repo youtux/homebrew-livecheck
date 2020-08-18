316f51a107ebe479072299905d3d70ef2cd05a03
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-18 14:48:45 -0400

    onioncat: update url

--- Livecheckables/onioncat.rb
+++ Livecheckables/onioncat.rb
@@ -1,6 +1,6 @@
 class Onioncat
   livecheck do
-    url "https://www.cypherpunk.at/ocat/download/Source/stable/"
+    url "https://www.cypherpunk.at/ocat/download/Source/current/"
     regex(/href=.*?onioncat[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
