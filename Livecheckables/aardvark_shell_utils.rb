64cd7647860fac4a1f3e03a028c2ca2a0c06cdf4
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-18 14:57:24 -0400

    aardvark_shell_utils: update url and regex

--- Livecheckables/aardvark_shell_utils.rb
+++ Livecheckables/aardvark_shell_utils.rb
@@ -1,6 +1,8 @@
 class AardvarkShellUtils
+  # This regex is multiline since there's a line break between `href=` and the
+  # attribute value on the homepage.
   livecheck do
-    url "http://downloads.laffeycomputer.com/current_builds/shellutils/"
-    regex(/href=.*?aardvark_shell_utils[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    url :homepage
+    regex(/href=.*?aardvark_shell_utils[._-]v?(\d+(?:\.\d+)+)\.t/im)
   end
 end
