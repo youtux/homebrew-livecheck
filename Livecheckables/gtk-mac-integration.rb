bfa9421ec89bdfc2e02b3e279a0f1261bce49643
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-18 20:19:27 -0400

    Gnome: remove DEV_VERSION_ALLOWLIST

--- Livecheckables/gtk-mac-integration.rb
+++ Livecheckables/gtk-mac-integration.rb
@@ -1,5 +1,8 @@
 class GtkMacIntegration
+  # We use a common regex because gtk-mac-integration doesn't use GNOME's
+  # "even-numbered minor is stable" version scheme.
   livecheck do
     url :stable
+    regex(/gtk-mac-integration[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
--- Livecheckables/libart.rb
+++ Livecheckables/libart.rb
@@ -1,5 +1,8 @@
 class Libart
+  # We use a common regex because libart doesn't use GNOME's "even-numbered
+  # minor is stable" version scheme.
   livecheck do
     url :stable
+    regex(/libart_lgpl[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
