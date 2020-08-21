3a7a0911914258ca118ff44f9e33ff0ce448b938
chenrui <rui@meetup.com>
2020-08-21 16:04:25 -0400

    carla: update url and regex
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Livecheckables/carla.rb
+++ Livecheckables/carla.rb
@@ -1,6 +1,6 @@
 class Carla
   livecheck do
-    url :head
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    url "https://github.com/falkTX/Carla/releases/latest"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 end
