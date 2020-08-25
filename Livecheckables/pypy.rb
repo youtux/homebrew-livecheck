b1d1bf32b98dd818a7cf42d2beb27d8ce28fe197
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-25 15:13:22 -0400

    pypy, pypy3: update url and add regex

--- Livecheckables/pypy.rb
+++ Livecheckables/pypy.rb
@@ -1,5 +1,6 @@
 class Pypy
   livecheck do
-    url :stable
+    url "https://downloads.python.org/pypy/"
+    regex(/href=.*?pypy2(?:\.\d+)*[._-]v?(\d+(?:\.\d+)+)-src\.t/i)
   end
 end
--- Livecheckables/pypy3.rb
+++ Livecheckables/pypy3.rb
@@ -1,5 +1,6 @@
 class Pypy3
   livecheck do
-    url :stable
+    url "https://downloads.python.org/pypy/"
+    regex(/href=.*?pypy3(?:\.\d+)*[._-]v?(\d+(?:\.\d+)+)-src\.t/i)
   end
 end
