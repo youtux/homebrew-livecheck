23de73d5a3b06747ac021488250b3bd4b4ddb807
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-25 17:15:38 -0400

    atlassian-cli: update url

--- Livecheckables/atlassian-cli.rb
+++ Livecheckables/atlassian-cli.rb
@@ -1,6 +1,6 @@
 class AtlassianCli
   livecheck do
-    url "https://marketplace.atlassian.com/plugins/org.swift.atlassian.cli/versions"
+    url "https://marketplace.atlassian.com/apps/10886/atlassian-command-line-interface-cli/version-history"
     regex(/class="version">v?(\d+(?:\.\d+)+)</i)
   end
 end
